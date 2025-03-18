f088d0fe1eb556313ff32b6c0dffe84ebed413a3
FP <FP@FP999>
2025-03-18 21:03:57 +0800

    j

---
+++ JavaOCP/src/day01/ArrayDemo7.java
@@ -0,0 +1,25 @@
+package day01;
+
+public class ArrayDemo7 {
+	public static void main(String[] args) {
+		int[] scoresA = {100, 80, 60};
+		int[] scoresB = {40, 45, 50};
+		// 求 scoreA 的平均
+		int sumA = 0;
+		for(int x : scoresA) {
+			sumA = sumA + x;
+		}
+		int avgA = sumA / scoresA.length;
+		// ---------------------------------
+		// 求 scoreB 的平均
+		int sumB = 0;
+		for(int x : scoresB) {
+			sumB = sumB + x;
+		}
+		int avgB = sumB / scoresB.length;
+		// ---------------------------------
+		
+		
+	}
+	
+}
