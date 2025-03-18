e485c36e019b80437d35b1b2b0045c7724a223aa
FP <FP@FP999>
2025-03-18 21:13:36 +0800

    j

--- JavaOCP/src/day01/ArrayDemo7.java
+++ JavaOCP/src/day01/ArrayDemo7.java
@@ -20,7 +20,21 @@ public class ArrayDemo7 {
 		// ---------------------------------
 		System.out.println("scoreA 的平均:" + avgA);
 		System.out.println("scoreB 的平均:" + avgB);
-		
+		// 計算 delatA 與 sdA -------------
+		double delatA = 0;
+		for(int x : scoresA) {
+			delatA = delatA + Math.pow(x - avgA, 2);
+		}
+		double sdA = Math.sqrt(delatA / scoresA.length); // scoresA 的標準差
+		// 計算 delatB 與 sdB -------------
+		double deltaB = 0;
+		for(int x : scoresB) {
+			deltaB = deltaB + Math.pow(x - avgB, 2);
+		}
+		double sdB = Math.sqrt(deltaB / scoresB.length); // scoresB 的標準差
+		// -------------------------------------------------------------------
+		System.out.println("scoresA 的標準差:" + sdA);
+		System.out.println("scoresB 的標準差:" + sdB);
 	}
 	
 }
