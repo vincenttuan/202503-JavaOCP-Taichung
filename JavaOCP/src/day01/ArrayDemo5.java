258560a3466be3cbabbe54cce950b10e793cc558
FP <FP@FP999>
2025-03-18 19:51:57 +0800

    j

---
+++ JavaOCP/src/day01/ArrayDemo5.java
@@ -0,0 +1,15 @@
+package day01;
+
+import java.util.Arrays;
+
+public class ArrayDemo5 {
+
+	public static void main(String[] args) {
+		// 班上有 10 位同學
+		// 如何記錄每一個同學的成績
+		int[] scores = {80, 70, 100, 90, 40, 60, 50, 30, 95, 55};
+		// 請計算總分與平均
+		
+	}
+
+}
