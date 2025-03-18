5f30e970fd70af04e9952a49e84941960d2520d7
FP <FP@FP999>
2025-03-18 20:44:53 +0800

    j

--- JavaOCP/src/day01/ArrayDemo6.java
+++ JavaOCP/src/day01/ArrayDemo6.java
@@ -8,12 +8,17 @@ public class ArrayDemo6 {
 		int[] scores = {80, 70, 100, 90, 40, 60, 50, 30, 95, 55};
 		// 請求最大值(最高分)
 		int max = scores[0];
+		int min = scores[0];
 		for(int i=1;i<scores.length;i++) {
 			if(scores[i] > max) {
 				max = scores[i]; // 替換
 			}
+			if(scores[i] < min) {
+				min = scores[i]; // 替換
+			}
 		}
 		System.out.println("最高分:" + max);
+		System.out.println("最低分:" + min);
 		
 	}
 
