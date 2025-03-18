72b86734d8636f60413707376ff26407b7147e91
FP <FP@FP999>
2025-03-18 19:58:02 +0800

    j

--- JavaOCP/src/day01/ArrayDemo5.java
+++ JavaOCP/src/day01/ArrayDemo5.java
@@ -9,7 +9,13 @@ public class ArrayDemo5 {
 		// 如何記錄每一個同學的成績
 		int[] scores = {80, 70, 100, 90, 40, 60, 50, 30, 95, 55};
 		// 請計算總分與平均
-		
+		int sum = 0; // 用來做資料累計使用
+		for(int x : scores) {
+			System.out.println(x); // x 表示每一筆分數
+			sum = sum + x; // 累計
+		}
+		System.out.println("--------");
+		System.out.println("總分:" + sum);
 	}
 
 }
