57664207b4bf5baf0d7e76b6fc34b62ab6b0d661
FP <FP@FP999>
2025-03-20 21:17:09 +0800

    j

--- JavaOCP/src/day02/Outfit.java
+++ JavaOCP/src/day02/Outfit.java
@@ -7,17 +7,19 @@ public class Outfit {
 	Top top; // 上衣
 	Bottom bottom; // 褲子/裙子
 	Hat hat; // 帽子
+	int price; // 組合的總價 
 	
 	public Outfit(String name, Top top, Bottom bottom, Hat hat) {
 		this.name = name;
 		this.top = top;
 		this.bottom = bottom;
 		this.hat = hat;
+		// 計算組合總價
+		this.price = top.price + bottom.price + hat.price;
 	}
 	
 	public void printInfo() {
-		int total = top.price + bottom.price + hat.price;
-		System.out.printf("風格名稱:%s 總價:$%d%n", name, total);
+		System.out.printf("風格名稱:%s 總價:$%d%n", name, price);
 		top.printInfo();
 		bottom.printInfo();
 		hat.printInfo();
