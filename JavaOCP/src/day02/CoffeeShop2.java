b02d87c91208de898aee4c25c5f29eb4f43d5a93
FP <FP@FP999>
2025-03-20 19:14:19 +0800

    j

--- JavaOCP/src/day02/CoffeeShop2.java
+++ JavaOCP/src/day02/CoffeeShop2.java
@@ -4,9 +4,13 @@ public class CoffeeShop2 {
 
 	public static void main(String[] args) {
 		// 建立一個咖啡實體同時也設定商品資訊
-		Coffee latte = new Coffee("拿鐵", '中', 65, true); 
+		Coffee c1 = new Coffee("拿鐵", '中', 65, true); 
+		Coffee c2 = new Coffee("焦糖瑪奇朵", '大', 80, false); 
+		Coffee c3 = new Coffee("美式", '大', 50, true); 
+		Coffee c4 = new Coffee("摩卡", '中', 60, true); 
+		Coffee c5 = new Coffee("義式濃縮", '小', 55, true); 
+		Coffee c6 = new Coffee("巧克力脆片", '大', 120, false);
 		
-		latte.printInfo();
 	}
 
 }
