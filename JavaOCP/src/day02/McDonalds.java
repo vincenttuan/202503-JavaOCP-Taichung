0c4447f0a712ccfd15f6fa1ed4a9442ce3a2dd5d
FP <FP@FP999>
2025-03-20 20:03:52 +0800

    j

--- JavaOCP/src/day02/McDonalds.java
+++ JavaOCP/src/day02/McDonalds.java
@@ -20,8 +20,16 @@ public class McDonalds {
 		for(int i=0;i<fastFoods.length;i++) {
 			fastFoods[i].printInfo();
 		}
-		
-		
+		// 套餐菜單
+		System.out.println("== 套餐 ========");
+		System.out.println("一號餐");
+		int total1 = 0;
+		for(int i=0;i<no1.length;i++) {
+			no1[i].printInfo();
+			total1 += no1[i].price;
+		}
+		System.out.printf("總價 $%d%n", total1);
+		System.out.println("---------------");
 		
 	}
 	
