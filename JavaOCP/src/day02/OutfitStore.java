350e9147184b63cb60307f38da2c6eea1a45706d
FP <FP@FP999>
2025-03-20 21:05:17 +0800

    j

--- JavaOCP/src/day02/OutfitStore.java
+++ JavaOCP/src/day02/OutfitStore.java
@@ -4,6 +4,7 @@ package day02;
 public class OutfitStore {
 	
 	public static void main(String[] args) {
+		// 單品
 		Top top1 = new Top("T-shirt 簡約白", 500);
 		Top top2 = new Top("Sweater 秋冬灰", 600);
 		Top top3 = new Top("Shirt 紳士黑", 1000);
@@ -16,11 +17,16 @@ public class OutfitStore {
 		Hat hat2 = new Hat("Hard Cap 安全帽", 900);
 		Hat hat3 = new Hat("Fedora 紳士帽", 1300);
 		
+		// 組合品
 		Outfit outfit1 = new Outfit("運動風", top1, bottom1, hat1);
 		Outfit outfit2 = new Outfit("安全風", top2, bottom2, hat2);
 		Outfit outfit3 = new Outfit("紳士風", top3, bottom3, hat3);
 		
-		
+		// 購物車
+		Outfit[] outfits = {outfit1, outfit2, outfit3};
+		for(int i=0;i<outfits.length;i++) {
+			outfits[i].printInfo();
+		}
 		
 		
 		
