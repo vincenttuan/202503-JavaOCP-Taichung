009eeb748d87536b0a7faecc9bdaf69cb966b59a
FP <FP@FP999>
2025-03-20 21:02:51 +0800

    j

--- JavaOCP/src/day02/OutfitStore.java
+++ JavaOCP/src/day02/OutfitStore.java
@@ -8,7 +8,20 @@ public class OutfitStore {
 		Top top2 = new Top("Sweater 秋冬灰", 600);
 		Top top3 = new Top("Shirt 紳士黑", 1000);
 		
-		Bottom
+		Bottom bottom1 = new Bottom("Jeans 牛仔藍", 800);
+		Bottom bottom2 = new Bottom("Shorts 運動灰", 400);
+		Bottom bottom3 = new Bottom("Pants 正式黑", 1800);
+		
+		Hat hat1 = new Hat("Cap 運動帽", 300);
+		Hat hat2 = new Hat("Hard Cap 安全帽", 900);
+		Hat hat3 = new Hat("Fedora 紳士帽", 1300);
+		
+		Outfit outfit1 = new Outfit("運動風", top1, bottom1, hat1);
+		Outfit outfit2 = new Outfit("安全風", top2, bottom2, hat2);
+		Outfit outfit3 = new Outfit("紳士風", top3, bottom3, hat3);
+		
+		
+		
 		
 		
 	}
