fb34fab0173a4433ff6dc982a3fd3481189fbfb7
FP <FP@FP999>
2025-03-20 20:54:22 +0800

    j

--- JavaOCP/src/day02/Outfit.java
+++ JavaOCP/src/day02/Outfit.java
@@ -3,20 +3,25 @@ package day02;
 // 全套服裝設計 (組合)
 public class Outfit {
 	
+	String name; // 風格名
 	Top top; // 上衣
 	Bottom bottom; // 褲子/裙子
 	Hat hat; // 帽子
 	
-	public Outfit(Top top, Bottom bottom, Hat hat) {
+	public Outfit(String name, Top top, Bottom bottom, Hat hat) {
+		this.name = name;
 		this.top = top;
 		this.bottom = bottom;
 		this.hat = hat;
 	}
 	
 	public void printInfo() {
+		int total = top.price + bottom.price + hat.price;
+		System.out.printf("風格名稱:%s 總價:$%d%n", name, total);
 		top.printInfo();
 		bottom.printInfo();
 		hat.printInfo();
+		System.out.println("---------------------");
 	}
 	
 }
---
+++ JavaOCP/src/day02/OutfitStore.java
@@ -0,0 +1,16 @@
+package day02;
+
+// 服飾店
+public class OutfitStore {
+	
+	public static void main(String[] args) {
+		Top top1 = new Top("T-shirt 簡約白", 500);
+		Top top2 = new Top("Sweater 秋冬灰", 600);
+		Top top3 = new Top("Shirt 紳士黑", 1000);
+		
+		Bottom
+		
+		
+	}
+	
+}
