edf369de7f17bfee90799cbbd83dca8b0c4d9ce2
FP <FP@FP999>
2025-03-25 19:06:24 +0800

    j

---
+++ JavaOCP/src/day03/StringDemo.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// 字串 concat 的應用
+		String s1 = "Java";
+		s1 = s1.concat("8");
+		System.out.println(s1);
+		
+		String s2 = "Java";
+		//s2 = s2.concat("11");
+		s2.concat("11");
+		System.out.println(s2);
+	}
+
+}
