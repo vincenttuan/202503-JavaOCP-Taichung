a3788cb74b3716d50985e89bf85963357ca36757
FP <FP@FP999>
2025-03-25 20:58:06 +0800

    j

---
+++ JavaOCP/src/day03/MultiArrayDemo3.java
@@ -0,0 +1,10 @@
+package day03;
+
+public class MultiArrayDemo3 {
+	public static void main(String[] args) {
+		// values 內含身高與體重的資訊
+		double[][] values = {{170.0, 182.5, 165.0}, {60.5, 72.5, 45.0}};
+		// 請計算這三組 bmi 各為多少 ?
+		
+	}
+}
