bd7b2074a2af8a0a947f257d2b02bcff0498719a
FP <FP@FP999>
2025-03-25 19:15:02 +0800

    j

---
+++ JavaOCP/src/day03/StringDemo2.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class StringDemo2 {
+
+	public static void main(String[] args) {
+		// StringBuffer vs StringBuilder(Java 5.0)
+		// 多執行緒下(慢)   單工模式下(快)
+		StringBuilder sb = new StringBuilder("Java");
+		//sb = sb.append("17").append("!");
+		sb.append("17").append("!");
+		System.out.println(sb); // sb.toString()
+
+	}
+
+}
