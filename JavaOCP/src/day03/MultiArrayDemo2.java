5c7f4d0bae1bd78996037f55dce910b29da0d107
FP <FP@FP999>
2025-03-25 19:37:54 +0800

    j

--- JavaOCP/src/day03/MultiArrayDemo2.java
+++ JavaOCP/src/day03/MultiArrayDemo2.java
@@ -6,6 +6,14 @@ public class MultiArrayDemo2 {
 		char[][] ttt = {{'O', 'X', 'X'}, {'X', 'O', ' '}, {'O', ' ', ' '}};
 		// 請問 'O' 要放在哪裡就會贏  ?
 		// 並印出棋盤
+		System.out.println(ttt[0]); // chat 陣列就是字串
+		System.out.println(ttt[1]);
+		System.out.println(ttt[2]);
+		System.out.println("----");
+		ttt[2][2] = 'O';
+		System.out.println(ttt[0]);
+		System.out.println(ttt[1]);
+		System.out.println(ttt[2]);
 		
 	}
 }
