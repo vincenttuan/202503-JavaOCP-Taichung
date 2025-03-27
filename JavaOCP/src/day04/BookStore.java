734539e989f7d11b34aa4e16da24490bb85606da
FP <FP@FP999>
2025-03-27 19:45:27 +0800

    j

--- JavaOCP/src/day04/BookStore.java
+++ JavaOCP/src/day04/BookStore.java
@@ -4,9 +4,9 @@ public class BookStore {
 
 	public static void main(String[] args) {
 		Book book1 = new Book();
-		book1.name = "現代Java";
+		book1.setName("現代Java");
 		book1.setPrice(450);
-		book1.pages = 325;
+		book1.setPages(325);
 		book1.printInfo();
 		// 單獨可以印出價格
 		System.out.println(book1.getPrice());
