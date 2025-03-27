aae6bcd72c7345bdbc9e84b4dc800af54752b08f
FP <FP@FP999>
2025-03-27 20:48:41 +0800

    j

--- JavaOCP/src/day04/Account.java
+++ JavaOCP/src/day04/Account.java
@@ -3,21 +3,34 @@ package day04;
 // 帳戶
 public class Account {
 	
-	private int balance; // 帳戶餘額
+	private int balance = 10000; // 帳戶餘額(含初始值)
+	
+	/*
+	public Account() {
+		balance = 10000;
+	}
+	*/
 	
 	// 存款
 	public void deposit(int amount) {
-		
+		if(amount <= 0) {
+			System.out.println("存款資料不正確: " + amount);
+			return;
+		}
+		balance += amount;
 	}
 	
 	// 提款
 	public void withdraw(int amount) {
-		
+		if(amount <= 0) {
+			System.out.println("提款資料不正確: " + amount);
+		}
+		balance -= amount;
 	}
 	
 	// 列印帳戶資料
 	public void printInfo() {
-		
+		System.out.printf("帳戶餘額: %,d%n", balance);
 	}
 	
 }
