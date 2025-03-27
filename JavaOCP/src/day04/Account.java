646424d8b8201535caeb387607cf92f8256e94d3
FP <FP@FP999>
2025-03-27 20:41:45 +0800

    j

---
+++ JavaOCP/src/day04/Account.java
@@ -0,0 +1,23 @@
+package day04;
+
+// 帳戶
+public class Account {
+	
+	private int balance; // 帳戶餘額
+	
+	// 存款
+	public void deposit(int amount) {
+		
+	}
+	
+	// 提款
+	public void withdraw(int amount) {
+		
+	}
+	
+	// 列印帳戶資料
+	public void printInfo() {
+		
+	}
+	
+}
