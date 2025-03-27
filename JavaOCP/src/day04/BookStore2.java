c4bafa2be205fe2567c669189f864f7df0a9343e
FP <FP@FP999>
2025-03-27 19:59:50 +0800

    j

---
+++ JavaOCP/src/day04/BookStore2.java
@@ -0,0 +1,15 @@
+package day04;
+
+public class BookStore2 {
+
+	public static void main(String[] args) {
+		Book book1 = new Book("現代Java", 450, 325);
+		Book book2 = new Book("唐詩300首", 300, 150);
+		Book book3 = new Book("兒歌帶動唱", 700, 250);
+		
+		Book[] books = {book1, book2, book3};
+		// 請問總共要付多少錢 ?
+
+	}
+
+}
