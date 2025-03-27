d78fe10c90ad87cbd511e26556ef7f3ef7e59a2f
FP <FP@FP999>
2025-03-27 20:34:47 +0800

    j

--- JavaOCP/src/day04/BookStore2.java
+++ JavaOCP/src/day04/BookStore2.java
@@ -9,6 +9,13 @@ public class BookStore2 {
 		
 		Book[] books = {book1, book2, book3};
 		// 請問總共要付多少錢 ?
+		int total = 0;
+		for(Book book : books) {
+			System.out.println(book.getPrice());
+			total += book.getPrice();
+		}
+		System.out.println("------");
+		System.out.println(total);
 
 	}
 
