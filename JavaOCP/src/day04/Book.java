74ac069af2c6a6f78614be20179a8a0a10f0147b
FP <FP@FP999>
2025-03-27 19:44:35 +0800

    j

--- JavaOCP/src/day04/Book.java
+++ JavaOCP/src/day04/Book.java
@@ -2,9 +2,9 @@ package day04;
 
 // 書的設計
 public class Book {
-	String name; // 書名
+	private String name; // 書名
 	private int price; // 價格
-	int pages; // 頁數
+	private int pages; // 頁數
 	
 	//若沒寫建構子則 Java 會自動產生, 所產生的建構子稱為預設建構子, 其結構如下所述:
 	// 無參數建構子
@@ -13,6 +13,18 @@ public class Book {
 	}
 	
 	// 封裝 setter
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPages(int pages) {
+		if(pages > 0) {
+			this.pages = pages;
+		} else {
+			System.out.println("頁數輸入錯誤");
+		}
+	}
+	
 	public void setPrice(int price) {
 		if(price > 0) {
 			this.price = price;
@@ -22,6 +34,14 @@ public class Book {
 	}
 	
 	// 封裝 getter
+	public String getName() {
+		return name;
+	}
+	
+	public int getPages() {
+		return pages;
+	}
+	
 	public int getPrice() {
 		//return this.price;
 		return price;
