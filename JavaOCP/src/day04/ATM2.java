bc719e49b588680c9be784ba5a5e484db178d81e
FP <FP@FP999>
2025-03-27 21:19:26 +0800

    j

--- JavaOCP/src/day04/ATM2.java
+++ JavaOCP/src/day04/ATM2.java
@@ -6,7 +6,11 @@ public class ATM2 {
 		Account account1 = new Account();
 		Account account2 = new Account();
 		
+		// account1 轉 4000 給 account2
+		account1.transfer(4000, account2);
 		
+		account1.printInfo();
+		account2.printInfo();
 		
 	}
 
