c341a1e6b48ffbc6ef29e7bf3ccd62c3b26d61f6
FP <FP@FP999>
2025-03-27 19:28:33 +0800

    j

--- JavaOCP/src/day04/Book.java
+++ JavaOCP/src/day04/Book.java
@@ -3,13 +3,22 @@ package day04;
 // 書的設計
 public class Book {
 	String name; // 書名
-	int price; // 價格
+	private int price; // 價格
 	int pages; // 頁數
 	
 	//若沒寫建構子則 Java 會自動產生, 所產生的建構子稱為預設建構子, 其結構如下所述:
 	// 無參數建構子
 	public Book() {
 		System.out.println("呼叫 Book() 建構子");
+	}
+	
+	// 封裝
+	public void setPrice(int p) {
+		if(p > 0) {
+			price = p;
+		} else {
+			System.out.println("價格輸入錯誤");
+		}
 	}
 	
 	public void printInfo() {
