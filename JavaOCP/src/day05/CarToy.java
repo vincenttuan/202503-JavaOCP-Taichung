2d7f58abfd205833eda00eca6bbc2da7ac86533e
FP <FP@FP999>
2025-04-01 18:53:21 +0800

    j

--- JavaOCP/src/day05/CarToy.java
+++ JavaOCP/src/day05/CarToy.java
@@ -2,5 +2,9 @@ package day05;
 
 // 車車
 public class CarToy {
-
+	private String name;
+	private int price;
+	private Wheel[] wheels;
+	private Battery battery;
+	
 }
--- JavaOCP/src/day05/DollToy.java
+++ JavaOCP/src/day05/DollToy.java
@@ -1,5 +1,8 @@
 package day05;
 
+// 洋娃娃
 public class DollToy {
-
+	private String name;
+	private int price;
+	private Battery battery;
 }
