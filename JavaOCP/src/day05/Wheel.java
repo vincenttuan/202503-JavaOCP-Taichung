96d5a99c37649d0a171a0433daa289b4a5fe4227
FP <FP@FP999>
2025-04-01 19:06:38 +0800

    j

--- JavaOCP/src/day05/Wheel.java
+++ JavaOCP/src/day05/Wheel.java
@@ -2,5 +2,24 @@ package day05;
 
 // 車輪
 public class Wheel {
+	private int size; // 3, 5, 7吋
+	private int price;
+	
+	public Wheel(int size, int price) {
+		this.size = size;
+		this.price = price;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public String toString() {
+		return String.format("Wheel size:%d吋 price:%d", size, price);
+	}
 	
 }
