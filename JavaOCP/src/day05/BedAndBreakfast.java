ae1cf0b292fab9ab100e6b9f9845dadbd9a8c6a0
FP <FP@FP999>
2025-04-01 21:15:27 +0800

    j

---
+++ JavaOCP/src/day05/BedAndBreakfast.java
@@ -0,0 +1,28 @@
+package day05;
+
+// 民宿
+public class BedAndBreakfast {
+
+	public static void main(String[] args) {
+		// 服務與餐點
+		Transport airport = new Transport("機場接送", 500);
+		Transport transtation = new Transport("高鐵接送", 200);
+		Transport self = new Transport("自行前往", 20);
+		Breakfast breakfastA = new Breakfast(150);
+		Breakfast breakfastB = new Breakfast(350);
+		// 建立房間
+		Room room1 = new Room("南洋風", 4, 4500, breakfastB, airport);
+		Room room2 = new Room("工業風", 2, 2000, breakfastA, self);
+		// 房型資料列印出
+		Object[] rooms = {room1, room2};
+		for(Object room : rooms) {
+			System.out.println(room);
+		}
+		System.out.println("----------------------------------");
+		
+		
+		
+
+	}
+
+}
