6a0ce053da1613d26031ed9fc39c39d66a80441f
FP <FP@FP999>
2025-04-01 19:15:13 +0800

    j

--- JavaOCP/src/day05/DollToy.java
+++ JavaOCP/src/day05/DollToy.java
@@ -6,4 +6,35 @@ public class DollToy {
 	private int price;
 	private Battery battery;
 	
+	public DollToy(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public DollToy(String name, int price, Battery battery) {
+		this.name = name;
+		this.price = price + battery.getPrice();
+		this.battery = battery;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public Battery getBattery() {
+		return battery;
+	}
+	
+	public String toString() {
+		if(battery == null) {
+			return String.format("DollToy name:%s price:%d", name, price);
+		}
+		return String.format("DollToy name:%s price:%d battery:%s", name, price, battery);
+		
+	}
+	
 }
