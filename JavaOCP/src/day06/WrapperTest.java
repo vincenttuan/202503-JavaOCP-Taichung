d9ddc66bbc8a7c6d600dd0ad8de71f862000b62f
FP <FP@FP999>
2025-04-08 19:37:58 +0800

    j

---
+++ JavaOCP/src/day06/WrapperTest.java
@@ -0,0 +1,13 @@
+package day06;
+
+public class WrapperTest {
+
+	public static void main(String[] args) {
+		int a = 10;
+		Integer b = Integer.valueOf(10);
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
