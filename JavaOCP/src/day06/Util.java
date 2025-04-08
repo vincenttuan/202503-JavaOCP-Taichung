5e821f599aff653a927ea5b61aadcdbfe5fc088b
FP <FP@FP999>
2025-04-08 19:04:45 +0800

    j

--- JavaOCP/src/day06/Util.java
+++ JavaOCP/src/day06/Util.java
@@ -3,6 +3,16 @@ package day06;
 // 工具
 public class Util {
 	
+	private static Util util = new Util();
+	
+	private Util() {
+		
+	}
+	
+	public static Util getInstance() {
+		return util;
+	}
+	
 	public int add(int... array) {
 		int total = 0;
 		for(int row : array) {
--- JavaOCP/src/day06/UtilTest.java
+++ JavaOCP/src/day06/UtilTest.java
@@ -4,7 +4,7 @@ public class UtilTest {
 
 	public static void main(String[] args) {
 		// 建立工具物件
-		Util util = new Util();
+		Util util = Util.getInstance();
 		// 進行 add 計算並可以得到結果後印出
 		System.out.println(util.add(10, 20));
 		System.out.println(util.add(10, 20, 30));
@@ -12,6 +12,15 @@ public class UtilTest {
 		System.out.println(util.add(array));
 		System.out.println(util.add());
 		System.out.println(util.add(1, 2, 3, 4, 5, 6, 7, 8, 9));
+		
+		Util util2 = Util.getInstance();
+		System.out.println(util2.add(10, 20, 30));
+		
+		Util util3 = Util.getInstance();
+		System.out.println(util3.add(10, 20, 30));
+		
+		
+		
 	}
 
 }
