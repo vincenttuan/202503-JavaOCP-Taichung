e64f2d3d9ddc45567013021cb4e23e73cc27e1bb
FP <FP@FP999>
2025-04-08 19:57:57 +0800

    j

--- JavaOCP/src/day06/WrapperTest.java
+++ JavaOCP/src/day06/WrapperTest.java
@@ -16,6 +16,18 @@ public class WrapperTest {
 		int scoreB = 90;
 		int highScore = Integer.max(scoreA, scoreB);
 		System.out.println(highScore);
+		// -----------------------------------------
+		// auto-boxing 自動裝箱
+		// Java 5.0 以前
+		Integer x1 = Integer.valueOf(10);
+		// Java 5.0(含) 以後
+		Integer x2 = 10; // auto-boxing 會自動將 10 轉成物件 Integer.valueOf(10)
+		
+		// auto-unboxing 自動拆箱
+		// Java 5.0 以前
+		System.out.println(x1.intValue());
+		// Java 5.0(含) 以後
+		System.out.println(x2); // auto-unboxing 會自動將 x2 物件內容透過 x2.intValue() 輸出 
 		
 	}
 
