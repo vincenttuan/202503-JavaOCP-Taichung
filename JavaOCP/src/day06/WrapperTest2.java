a32efede3b421e11564d95704f0e74a245000dbf
FP <FP@FP999>
2025-04-08 20:02:31 +0800

    j

--- JavaOCP/src/day06/WrapperTest2.java
+++ JavaOCP/src/day06/WrapperTest2.java
@@ -6,11 +6,15 @@ public class WrapperTest2 {
 		// auto-boxing / auto-unboxing 練習
 		Integer x1 = 10; // auto-boxing, Integer x1 = Integer.valueOf(10);
 		int x2 = x1;     // auto-unboxing, int x2 = x1.intValue();
+		Integer x3 = 10; // auto-boxing, Integer x3 = Integer.valueOf(10);
 		
 		System.out.println(x1);
 		System.out.println(x2);
+		System.out.println(x3);
 		System.out.println(x1 == x2);
-
+		System.out.println(x1 == x3);
+		
+		
 	}
 
 }
