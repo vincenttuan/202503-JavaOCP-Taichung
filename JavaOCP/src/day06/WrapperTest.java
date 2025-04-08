e976f4341dfd5801f364aaea73537d95fdd1faff
FP <FP@FP999>
2025-04-08 19:47:19 +0800

    j

--- JavaOCP/src/day06/WrapperTest.java
+++ JavaOCP/src/day06/WrapperTest.java
@@ -6,8 +6,17 @@ public class WrapperTest {
 		int a = 10;
 		Integer b = Integer.valueOf(10);
 		System.out.println(a);
-		System.out.println(b);
-
+		System.out.println(b.intValue());
+		
+		// 得到 int 得最大值 ?
+		int max = Integer.MAX_VALUE;
+		System.out.println(max);
+		
+		int scoreA = 70;
+		int scoreB = 90;
+		int highScore = Integer.max(scoreA, scoreB);
+		System.out.println(highScore);
+		
 	}
 
 }
