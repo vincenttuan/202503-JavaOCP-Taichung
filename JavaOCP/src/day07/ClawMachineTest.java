a87d67c859d20c87e3de141b1bfbccf7d8e42289
FP <FP@FP999>
2025-04-10 21:01:52 +0800

    j

---
+++ JavaOCP/src/day07/ClawMachineTest.java
@@ -0,0 +1,12 @@
+package day07;
+
+public class ClawMachineTest {
+
+	public static void main(String[] args) {
+		ClawMachine clawMachine = new ClawMachine();
+		clawMachine.insertCoin();
+		clawMachine.play();
+
+	}
+
+}
