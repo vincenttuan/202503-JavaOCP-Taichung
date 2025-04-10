b87388dfa41e11b30aa0550622845d404faaf2ce
FP <FP@FP999>
2025-04-10 19:03:52 +0800

    j

---
+++ JavaOCP/src/day06/Son2.java
@@ -0,0 +1,12 @@
+package day06;
+
+import day07.Father;
+
+public class Son2 extends Father {
+	public void info() {
+		//System.out.println(cash);
+		System.out.println(stock);
+		//System.out.println(super.cash);
+		System.out.println(super.stock);
+	}
+}
