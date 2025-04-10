cd7b22e17683a7a9a71a0fcbfea9c84fb9f24571
FP <FP@FP999>
2025-04-10 21:09:32 +0800

    j

--- JavaOCP/src/day07/ClawMachineTest.java
+++ JavaOCP/src/day07/ClawMachineTest.java
@@ -3,10 +3,20 @@ package day07;
 public class ClawMachineTest {
 
 	public static void main(String[] args) {
-		ClawMachine clawMachine = new ClawMachine();
+		//ClawMachine clawMachine = new ClawMachine();
+		ClawMachine clawMachine = new DeluxeClawMachine();
+		//--------------------------------------------------
 		clawMachine.insertCoin();
 		clawMachine.play();
 		clawMachine.play();
+		System.out.println("-------------------------");
+		
+		DeluxeClawMachine deluxeClawMachine = new DeluxeClawMachine();
+		deluxeClawMachine.insertCoin();
+		deluxeClawMachine.play();
+		deluxeClawMachine.play();
+		deluxeClawMachine.playMusic(); // DeluxeClawMachine 專屬方法
+		
 	}
 
 }
