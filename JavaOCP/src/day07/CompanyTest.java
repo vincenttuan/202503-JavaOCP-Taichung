bb236b159bb72e0ba10df57a9246caa54940d937
FP <FP@FP999>
2025-04-10 19:33:18 +0800

    j

--- JavaOCP/src/day07/CompanyTest.java
+++ JavaOCP/src/day07/CompanyTest.java
@@ -4,16 +4,16 @@ public class CompanyTest {
 
 	public static void main(String[] args) {
 		Employee employee = new Employee();
-		System.out.println(employee.salary);
+		System.out.println(employee.salary); // 50000
 		
 		Manager manager = new Manager();
-		System.out.println(manager.salary);
-		System.out.println(manager.budget);
+		System.out.println(manager.salary); // 50000
+		System.out.println(manager.budget); // 70000
 		
 		Supervisor supervisor = new Supervisor();
-		System.out.println(supervisor.salary);
-		System.out.println(supervisor.budget);
-		System.out.println(supervisor.stock);
+		System.out.println(supervisor.salary); // 50000
+		System.out.println(supervisor.budget); // 70000
+		System.out.println(supervisor.stock);  // 120000
 		
 	}
 
