b0b615e44431ffaf869960b76648693ef843a4b3
FP <FP@FP999>
2025-04-10 19:56:04 +0800

    j

--- JavaOCP/src/day07/Dog.java
+++ JavaOCP/src/day07/Dog.java
@@ -1,6 +1,6 @@
 package day07;
 
-public class Dog {
+public class Dog extends Animal {
 	
 	public Dog() {
 		System.out.println("執行 Dog 建構子");
---
+++ JavaOCP/src/day07/DogTest.java
@@ -0,0 +1,9 @@
+package day07;
+
+public class DogTest {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
