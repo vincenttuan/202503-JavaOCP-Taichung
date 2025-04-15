e1e0e0b0da95afe479b3aa9e6b4a08258343d7d3
FP <FP@FP999>
2025-04-15 19:15:30 +0800

    j

---
+++ JavaOCP/src/day08/SingStreetShow.java
@@ -0,0 +1,14 @@
+package day08;
+
+// 🎤🌟 街頭歌唱藝人
+public class SingStreetShow extends NightMarketActivity {
+	public SingStreetShow() {
+		cost = 30;
+	}
+
+	@Override
+	public void doActivity() {
+		System.out.println("🎤🌟 街頭歌唱藝人掌聲不斷👏👏👏~");
+	}
+	
+}
