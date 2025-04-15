0406609e30d98ac2cce503bcfc80c400e5739617
FP <FP@FP999>
2025-04-15 19:55:46 +0800

    j

---
+++ JavaOCP/src/day08/StudentTest.java
@@ -0,0 +1,30 @@
+package day08;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		NightMarketActivity eatBigWrapSmall = new EatBigWrapSmall();
+		NightMarketActivity drinkFrogEgg = new DrinkFrogEgg();
+		NightMarketActivity playBalloonGame = new PlayBalloonGame();
+		NightMarketActivity singStreetShow = new SingStreetShow();
+		
+		Student amy = new Student("Amy");
+		amy.addActivity(eatBigWrapSmall);
+		amy.addActivity(drinkFrogEgg);
+		amy.addActivity(playBalloonGame);
+		
+		Student ben = new Student("Ben");
+		ben.addActivity(new EatBigWrapSmall());
+		ben.addActivity(new DrinkFrogEgg());
+		ben.addActivity(new PlayBalloonGame());
+		ben.addActivity(new SingStreetShow());
+		ben.addActivity(new EatBigWrapSmall());
+		
+		amy.playNightMarketActivity();
+		ben.playNightMarketActivity();
+		
+		
+
+	}
+
+}
