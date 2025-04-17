80d3faabd71b5ed945871477d86aba0b2f8dd355
FP <FP@FP999>
2025-04-17 21:05:16 +0800

    j

--- JavaOCP/src/day09/case2/SnackStore2.java
+++ JavaOCP/src/day09/case2/SnackStore2.java
@@ -4,7 +4,8 @@ public class SnackStore2 {
 
 	public static void main(String[] args) {
 		SaltedChicken saltedChicken = new SaltedChicken("鹽酥雞", 50);
-		System.out.printf("%s 份量:%d 價格:%d%n", saltedChicken.getName());
+		System.out.printf("%s 份量:%d 價格:%d%n", 
+				saltedChicken.getName(), saltedChicken.getSize(), saltedChicken.getPrice());
 
 	}
 
