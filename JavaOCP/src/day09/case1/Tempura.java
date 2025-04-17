4885e527537689dd73b8bce5a5596c84958392e6
FP <FP@FP999>
2025-04-17 19:17:54 +0800

    j

--- JavaOCP/src/day09/case1/Tempura.java
+++ JavaOCP/src/day09/case1/Tempura.java
@@ -22,19 +22,18 @@ public class Tempura implements Snack {
 	
 	@Override
 	public void prepare() {
-		
+		System.out.println("將圓形甜不辣切條, 祖傳蜜醬汁");
 	}
 
 	@Override
 	public void cook() {
-		
+		System.out.println("炸到蓬鬆(油溫180度約4分鐘)之後加入蜜醬汁");
 	}
 
 	@Override
 	public void serve() {
-		
+		System.out.printf("將 %s 灑上胡椒粉後裝入紙袋");
 	}
-	
 	
 	
 }
