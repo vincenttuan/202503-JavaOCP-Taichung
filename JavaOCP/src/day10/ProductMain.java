03fc6f608b4f2ec989933b5a8d85bb0817759065
FP <FP@FP999>
2025-04-22 19:49:27 +0800

    j

---
+++ JavaOCP/src/day10/ProductMain.java
@@ -0,0 +1,29 @@
+package day10;
+
+import java.util.stream.Stream;
+
+public class ProductMain {
+
+	public static void main(String[] args) {
+		Toner toner = new Toner("玫瑰保濕化妝水", 850);
+		Lotion lotion = new Lotion("玻尿酸乳液", 1250);
+		Serum serum = new Serum("維他命C精華液", 1680);
+		
+		// 專屬方法示範
+		toner.refreshSkin();
+		lotion.moisture();
+		serum.repairSkin();
+		
+		// 價格
+		/*
+		System.out.printf("%s $%,d%n", toner.getName(), toner.getPrice());
+		System.out.printf("%s $%,d%n", lotion.getName(), lotion.getPrice());
+		System.out.printf("%s $%,d%n", serum.getName(), serum.getPrice());
+		*/
+		Product[] products = {toner, lotion, serum};
+		Stream.of(products)
+			  .forEach(p -> System.out.printf("%s $%,d%n", p.getName(), p.getPrice()));
+		
+	}
+
+}
