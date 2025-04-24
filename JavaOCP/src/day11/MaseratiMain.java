49faae55e37a81d8a65069f27973d3a70dfd7ff8
FP <FP@FP999>
2025-04-24 21:14:06 +0800

    j

--- JavaOCP/src/day11/MaseratiMain.java
+++ JavaOCP/src/day11/MaseratiMain.java
@@ -3,7 +3,17 @@ package day11;
 public class MaseratiMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 1.請列出瑪莎拉蒂的設備配置
+		
+		
+		// 2.請列出瑪莎拉蒂專屬的金融貸款服務
+		
+		
+		// 3.我要試駕
+		
+		
+		// 4.填寫反饋
+		
 
 	}
 
