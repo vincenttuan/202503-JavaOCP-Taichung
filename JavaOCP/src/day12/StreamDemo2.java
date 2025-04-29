5f844a95527cd8654dc8341c644eb7c03a27d719
FP <FP@FP999>
2025-04-29 21:17:32 +0800

    j

---
+++ JavaOCP/src/day12/StreamDemo2.java
@@ -0,0 +1,32 @@
+package day12;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+
+public class StreamDemo2 {
+	public static void main(String[] args) {
+		int[] scores = {45, 55, 76, 88, 90, 50, 10};
+		boolean check1 = Arrays.stream(scores)
+							   .allMatch(score -> score >= 0);
+		System.out.println(check1);
+		
+		boolean check2 = Arrays.stream(scores)
+				   			   .allMatch(score -> score >= 60);
+		System.out.println(check2);
+		
+		boolean check3 = Arrays.stream(scores)
+	   			   			   .noneMatch(score -> score >= 60);
+		System.out.println(check3);
+		
+		// 最大, 最小, 總和, 平均, 個數 => 統計資訊
+		IntSummaryStatistics stat = Arrays.stream(scores).summaryStatistics();
+		System.out.println(stat);
+		System.out.println(stat.getMax());
+		System.out.println(stat.getMin());
+		System.out.println(stat.getSum());
+		System.out.println(stat.getAverage());
+		System.out.println(stat.getCount());
+		
+
+	}
+}
