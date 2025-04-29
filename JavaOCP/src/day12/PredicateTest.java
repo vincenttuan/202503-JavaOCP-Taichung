61a4470637a563d346c804e41dc3252fc3c77bb1
FP <FP@FP999>
2025-04-29 19:50:43 +0800

    j

--- JavaOCP/src/day12/PredicateTest.java
+++ JavaOCP/src/day12/PredicateTest.java
@@ -1,11 +1,14 @@
 package day12;
 
+import java.util.function.Predicate;
+
 public class PredicateTest {
 
 	public static void main(String[] args) {
 		// 利用 Predicate 判斷成績是否及格 ?
-		
-
+		Predicate<Integer> check = (score) -> score >= 60;
+		System.out.println(check.test(85));
+		System.out.println(check.test(45));
 	}
 
 }
