52673bfcedc5022cedd8dbb9d6658a1a1e8aa84e
FP <FP@FP999>
2025-04-29 19:17:59 +0800

    j

---
+++ JavaOCP/src/day12/ConsumerDemo.java
@@ -0,0 +1,17 @@
+package day12;
+
+import java.util.function.Consumer;
+
+public class ConsumerDemo {
+
+	public static void main(String[] args) {
+		Consumer<Integer> c1 = (t) -> System.out.println(t+t);
+		c1.accept(30);
+		
+		Consumer<String> c2 = (t) -> System.out.println(t+t);
+		c2.accept("30");
+		
+
+	}
+
+}
