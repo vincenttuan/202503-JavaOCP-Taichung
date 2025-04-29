b8081590030a8cd9506c832d8eb31528f7bd1e12
FP <FP@FP999>
2025-04-29 21:06:26 +0800

    j

--- JavaOCP/src/day12/StreamDemo1.java
+++ JavaOCP/src/day12/StreamDemo1.java
@@ -8,14 +8,6 @@ public class StreamDemo1 {
 
 	public static void main(String[] args) {
 		int[] scores = {45, 55, 76, 88, 90, 50, 10};
-		// 找出一筆及格的分數 I
-		int passScore = Arrays.stream(scores)
-							  //.parallel() // 並行流
-							  .filter(score -> score >= 60)
-							  .findAny()
-							  .getAsInt();
-		System.out.println(passScore);
-		
 		// 找出一筆及格的分數 II
 		OptionalInt optInt = Arrays.stream(scores)
 								   .filter(score -> score >= 60)
