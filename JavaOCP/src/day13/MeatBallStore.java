23f6931bf724b5df7453f48a55afd03740075058
FP <FP@FP999>
2025-05-01 19:20:35 +0800

    j

--- JavaOCP/src/day13/MeatBallStore.java
+++ JavaOCP/src/day13/MeatBallStore.java
@@ -3,8 +3,8 @@ package day13;
 public class MeatBallStore {
 	
 	public static void main(String[] args) {
-		MeatBall meatBall1 = new MeatBall("豬肉", true, 55);
-		MeatBall meatBall2 = new MeatBall("豬肉", true, 55);
+		MeatBall meatBall1 = new MeatBall("牛肉", true, 55);
+		MeatBall meatBall2 = new MeatBall("牛肉", true, 55);
 		System.out.println(meatBall1); // 會自動呼叫 toString() 方法
 		System.out.println(meatBall2); // 會自動呼叫 toString() 方法
 		System.out.println(meatBall1 == meatBall2); // false
