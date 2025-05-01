570c4f3e8a8624e899c34428966362eb6f0bcdbc
FP <FP@FP999>
2025-05-01 19:50:10 +0800

    j

--- JavaOCP/src/day13/SoupStore.java
+++ JavaOCP/src/day13/SoupStore.java
@@ -3,8 +3,11 @@ package day13;
 public class SoupStore {
 	public static void main(String[] args) {
 		
-		SoupType soupType = SoupType.原味;
-		System.out.println(soupType.getDescription());
+		SoupType soupType1 = SoupType.原味;
+		System.out.println(soupType1.getDescription());
+		
+		SoupType soupType2 = SoupType.辣味;
+		System.out.println(soupType2.getDescription());
 		
 	}
 }
