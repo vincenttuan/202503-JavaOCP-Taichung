d9bb5b2dd3e21ef11ef969f0d984995dc90bc21f
FP <FP@FP999>
2025-05-06 21:18:19 +0800

    j

--- JavaOCP/src/day14/SetDemo5.java
+++ JavaOCP/src/day14/SetDemo5.java
@@ -11,13 +11,15 @@ public class SetDemo5 {
 		gifts.add(new Gift("按摩椅", "健康", 26800, "媽媽的健康無價"));
 		gifts.add(new Gift("資生堂抗老精華", "美妝", 3200, "媽媽永遠年輕美麗"));
 		gifts.add(new Gift("烤箱", "實用", 2500, "讓媽媽烤餅乾給我吃讓媽媽開心"));
-		gifts.add(new Gift("手寫卡片", "DIY", 0, "媽媽我愛你, 謝謝你的付出"));
+		gifts.add(new Gift("手寫卡片", "DIY", null, "媽媽我愛你, 謝謝你的付出"));
 		gifts.add(new Gift("紅包", "現金", 600, "媽媽請省點用 心意比較重要"));
 		System.out.println(gifts);
 		// 請問此次母親節總共花費多少 ?
 		int total = 0;
 		for(Gift gift : gifts) {
-			total += gift.getPrice();
+			if(gift.getPrice() != null) {
+				total += gift.getPrice();
+			}
 		}
 		System.out.printf("%,d%n", total);
 		//-----------------------------------------------------------------
