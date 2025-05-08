74c7b67783ab76ccd5424aeaf4add21a6a7140b4
FP <FP@FP999>
2025-05-08 21:24:12 +0800

    j

--- JavaOCP/src/day15/ListDemo1.java
+++ JavaOCP/src/day15/ListDemo1.java
@@ -12,7 +12,26 @@ public class ListDemo1 {
 		scores.add(100);
 		scores.add(100);
 		scores.add(100);
+		scores.add(1, 80); // 指定位置
 		System.out.println(scores);
+		// 取得元素
+		System.out.println(scores.get(0)); // 首筆
+		System.out.println(scores.get(scores.size() - 1)); // 末筆
+		System.out.println("-------");
+		// 走訪元素 I
+		for(int i=0, lens=scores.size();i<lens;i++) {
+			System.out.println(scores.get(i));
+		}
+		System.out.println("-------");
+		// 走訪元素 II
+		for(Integer score : scores) {
+			System.out.println(score);
+		}
+		System.out.println("-------");
+		// 走訪元素 III
+		//scores.forEach(System.out::println);
+		scores.forEach(score -> System.out.println(score));
+			
 	}
 	
 }
