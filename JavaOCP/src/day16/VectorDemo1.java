0b8d1fc6623fac8137bfb3ffa9770397c43f1ab7
FP <FP@FP999>
2025-05-13 21:14:31 +0800

    j

--- JavaOCP/src/day16/VectorDemo1.java
+++ JavaOCP/src/day16/VectorDemo1.java
@@ -6,12 +6,14 @@ public class VectorDemo1 {
 
 	public static void main(String[] args) {
 		//  Vector 執行緒安全的集合
-		Vector<Integer> v = new Vector<>();
-		for(int i=1;i<=10;i++) {
+		Vector<Integer> v = new Vector<>(500, 10);
+		for(int i=1;i<=500;i++) {
 			v.add(1);
 		}
+		v.add(1);
 		System.out.println("size: " + v.size());
 		System.out.println("capacity: " + v.capacity());
+		System.out.println("free: " + (v.capacity() - v.size()));
 		
 
 	}
