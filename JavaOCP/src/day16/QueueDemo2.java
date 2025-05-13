cc4626800a98b81e6aa4dd9d33b1607700866cff
FP <FP@FP999>
2025-05-13 19:06:24 +0800

    j

--- JavaOCP/src/day16/QueueDemo2.java
+++ JavaOCP/src/day16/QueueDemo2.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 
 public class QueueDemo2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Queue<String> patientQueue = new LinkedList<>(); // 病人掛號佇列
 		// 病人依序排隊看病
 		patientQueue.offer("劉一");
@@ -18,7 +18,19 @@ public class QueueDemo2 {
 		patientQueue.offer("周八");
 		patientQueue.offer("吳九");
 		patientQueue.offer("鄭十");
-
+		
+		System.out.println("排隊: " + patientQueue);
+		
+		// 模擬醫生看診
+		while (!patientQueue.isEmpty()) {
+			Thread.sleep(1000);
+			String name = patientQueue.poll();
+			System.out.println("叫號 " + name);
+			Thread.sleep(1000);
+			System.out.println("目前 " + name + " 正在看診... 目前排隊: " + patientQueue);
+		}
+		
+		System.out.println("排隊: " + patientQueue);
 	}
 
 }
