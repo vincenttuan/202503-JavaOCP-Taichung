0ed4921fa88fe725ace5381d929a22deb688d331
FP <FP@FP999>
2025-05-13 19:01:27 +0800

    j

---
+++ JavaOCP/src/day16/QueueDemo2.java
@@ -0,0 +1,24 @@
+package day16;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo2 {
+
+	public static void main(String[] args) {
+		Queue<String> patientQueue = new LinkedList<>(); // 病人掛號佇列
+		// 病人依序排隊看病
+		patientQueue.offer("劉一");
+		patientQueue.offer("陳二");
+		patientQueue.offer("張三");
+		patientQueue.offer("李四");
+		patientQueue.offer("王五");
+		patientQueue.offer("趙六");
+		patientQueue.offer("孫七");
+		patientQueue.offer("周八");
+		patientQueue.offer("吳九");
+		patientQueue.offer("鄭十");
+
+	}
+
+}
