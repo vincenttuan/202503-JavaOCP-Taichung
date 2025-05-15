112f6d843473a834f044512a1ce437207ec484f0
FP <FP@FP999>
2025-05-15 21:01:35 +0800

    j

---
+++ JavaOCP/src/day17/ListShuffleDemo.java
@@ -0,0 +1,19 @@
+package day17;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ListShuffleDemo {
+
+	public static void main(String[] args) {
+		// Shuffle: 洗牌 ♠♥♣♦
+		List<String> card1 = List.of("♠A", "♠2", "♠3", "♠4", "♠5", "♠6", "♠7", "♠8", "♠9", "♠10", "♠J", "♠Q", "♠K"); 
+		List<String> card2 = List.of("♥A", "♥2", "♥3", "♥4", "♥5", "♥6", "♥7", "♥8", "♥9", "♥10", "♥J", "♥Q", "♥K"); 
+		List<String> card3 = List.of("♦A", "♦2", "♦3", "♦4", "♦5", "♦6", "♦7", "♦8", "♦9", "♦10", "♦J", "♦Q", "♦K"); 
+		List<String> card4 = List.of("♣A", "♣2", "♣3", "♣4", "♣5", "♣6", "♣7", "♣8", "♣9", "♣10", "♣J", "♣Q", "♣K"); 
+		
+
+	}
+
+}
