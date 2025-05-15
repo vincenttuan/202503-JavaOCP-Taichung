ac6073881f9b7de7fb82ba384ebb923fe91b66d2
FP <FP@FP999>
2025-05-15 18:48:16 +0800

    j

---
+++ JavaOCP/src/day17/MapDemo1.java
@@ -0,0 +1,22 @@
+package day17;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapDemo1 {
+
+	public static void main(String[] args) {
+		Map<String, Integer> map = new LinkedHashMap<>();
+		map.put("國文", 95);
+		map.put("數學", 100);
+		map.put("社會", 100);
+		map.put("自然", 100);
+		System.out.println(map);
+		System.out.println(map.keySet()); // 所有的 key
+		System.out.println(map.values()); // 所有的 value
+		System.out.println(map.get("國文")); // O(1)
+		System.out.println(map.get("英文")); // O(1)
+		
+	}
+
+}
