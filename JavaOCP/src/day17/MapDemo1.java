7d9946733301c48751e7384fa9361c45b15243d5
FP <FP@FP999>
2025-05-15 19:09:04 +0800

    j

--- JavaOCP/src/day17/MapDemo1.java
+++ JavaOCP/src/day17/MapDemo1.java
@@ -21,7 +21,7 @@ public class MapDemo1 {
 			System.out.printf("科目: %s 分數: %d%n", entry.getKey(), entry.getValue());
 		}
 		
-		// 找出最高分的科目
+		// 找出最高分的科目: 傳統 for-loop
 		Map.Entry<String, Integer> highest = null;
 		for(Map.Entry<String, Integer> entry : map.entrySet()) {
 			if(highest == null || entry.getValue() > highest.getValue()) {
@@ -29,6 +29,11 @@ public class MapDemo1 {
 			}
 		}
 		System.out.println(highest.getKey());
+		// 找出最高分的科目: 現代 Stream API
+		map.entrySet()
+		   .stream()
+		   .max(Map.Entry.comparingByValue()) // 根據 value 來找最大值的元素(entry)
+		   .ifPresent(entry -> System.out.println(entry.getKey())); // 若有找到則印出 key 值
 	}
 
 }
