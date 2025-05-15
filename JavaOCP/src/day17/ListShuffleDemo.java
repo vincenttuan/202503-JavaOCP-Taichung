906e0767d7792d53437b9e00576fcb6592652eba
FP <FP@FP999>
2025-05-15 21:04:14 +0800

    j

--- JavaOCP/src/day17/ListShuffleDemo.java
+++ JavaOCP/src/day17/ListShuffleDemo.java
@@ -1,5 +1,6 @@
 package day17;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -12,8 +13,15 @@ public class ListShuffleDemo {
 		List<String> card2 = List.of("♥A", "♥2", "♥3", "♥4", "♥5", "♥6", "♥7", "♥8", "♥9", "♥10", "♥J", "♥Q", "♥K"); 
 		List<String> card3 = List.of("♦A", "♦2", "♦3", "♦4", "♦5", "♦6", "♦7", "♦8", "♦9", "♦10", "♦J", "♦Q", "♦K"); 
 		List<String> card4 = List.of("♣A", "♣2", "♣3", "♣4", "♣5", "♣6", "♣7", "♣8", "♣9", "♣10", "♣J", "♣Q", "♣K"); 
+		// 將 card1 ~ card4 合併到一個 List 中
+		List<String> allCards = new ArrayList<>();
+		allCards.addAll(card1);
+		allCards.addAll(card2);
+		allCards.addAll(card3);
+		allCards.addAll(card4);
+		System.out.println(allCards);
 		
-
+		
 	}
 
 }
