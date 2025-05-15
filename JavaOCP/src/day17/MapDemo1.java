24330696806bdddd080c0a2245bac223431c98ac
FP <FP@FP999>
2025-05-15 19:16:22 +0800

    j

--- JavaOCP/src/day17/MapDemo1.java
+++ JavaOCP/src/day17/MapDemo1.java
@@ -2,6 +2,9 @@ package day17;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import static java.util.Map.Entry;
+import static java.util.Map.Entry.comparingByValue;
+import static java.lang.System.out;
 
 public class MapDemo1 {
 
@@ -11,29 +14,29 @@ public class MapDemo1 {
 		map.put("數學", 100);
 		map.put("社會", 70);
 		map.put("自然", 80);
-		System.out.println(map);
-		System.out.println(map.keySet()); // 所有的 key
-		System.out.println(map.values()); // 所有的 value
-		System.out.println(map.get("國文")); // O(1)
-		System.out.println(map.get("英文")); // O(1)
+		out.println(map);
+		out.println(map.keySet()); // 所有的 key
+		out.println(map.values()); // 所有的 value
+		out.println(map.get("國文")); // O(1)
+		out.println(map.get("英文")); // O(1)
 		// 列出所有元素
-		for(Map.Entry<String, Integer> entry : map.entrySet()) {
-			System.out.printf("科目: %s 分數: %d%n", entry.getKey(), entry.getValue());
+		for(Entry<String, Integer> entry : map.entrySet()) {
+			out.printf("科目: %s 分數: %d%n", entry.getKey(), entry.getValue());
 		}
 		
 		// 找出最高分的科目: 傳統 for-loop
-		Map.Entry<String, Integer> highest = null;
-		for(Map.Entry<String, Integer> entry : map.entrySet()) {
+		Entry<String, Integer> highest = null;
+		for(Entry<String, Integer> entry : map.entrySet()) {
 			if(highest == null || entry.getValue() > highest.getValue()) {
 				highest = entry;
 			}
 		}
-		System.out.println(highest.getKey());
+		out.println(highest.getKey());
 		// 找出最高分的科目: 現代 Stream API
 		map.entrySet()
 		   .stream()
-		   .max(Map.Entry.comparingByValue()) // 根據 value 來找最大值的元素(entry)
-		   .ifPresent(entry -> System.out.println(entry.getKey())); // 若有找到則印出 key 值
+		   .max(comparingByValue()) // 根據 value 來找最大值的元素(entry)
+		   .ifPresent(entry -> out.println(entry.getKey())); // 若有找到則印出 key 值
 	}
 
 }
