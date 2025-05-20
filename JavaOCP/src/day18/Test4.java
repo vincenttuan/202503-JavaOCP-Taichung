f6e0ca04ebe64c0744f4b6ae3026b259f70ee35e
FP <FP@FP999>
2025-05-20 21:24:49 +0800

    j

--- JavaOCP/src/day18/Test4.java
+++ JavaOCP/src/day18/Test4.java
@@ -1,5 +1,6 @@
 package day18;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -15,14 +16,20 @@ public class Test4 {
 		// 用 Scanner 來與電腦玩剪刀石頭布
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("請輸入剪刀(0) 石頭(1) 布(2) => ");
-		int user = scanner.nextInt();
-		int pc = new Random().nextInt(3); // 0, 1, 2
-		System.out.printf("user => %s, pc => %s%n", gameMap.get(user), gameMap.get(pc));
-		// 判定勝負
-		int result = (user - pc + 3) % 3;
-		// 誰贏
-		System.out.println(winnerMap.get(result));
-
+		
+		try {
+			int user = scanner.nextInt();
+			int pc = new Random().nextInt(3); // 0, 1, 2
+			System.out.printf("user => %s, pc => %s%n", gameMap.get(user), gameMap.get(pc));
+			// 判定勝負
+			int result = (user - pc + 3) % 3;
+			// 誰贏
+			System.out.println(winnerMap.get(result));
+		} catch (InputMismatchException e) {
+			System.out.println("輸入資料不正確, 錯誤類型:" + e.getClass().getSimpleName());
+		}
+		
+		scanner.close();
 	}
 
 }
