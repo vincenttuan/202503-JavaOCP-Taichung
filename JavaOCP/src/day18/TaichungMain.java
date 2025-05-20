d8c6302b182fb441f4b8b5ac58d10d232271b1ef
FP <FP@FP999>
2025-05-20 19:39:17 +0800

    j

--- JavaOCP/src/day18/TaichungMain.java
+++ JavaOCP/src/day18/TaichungMain.java
@@ -1,6 +1,7 @@
 package day18;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class TaichungMain {
@@ -23,7 +24,10 @@ public class TaichungMain {
 		System.out.println("----------------------------------------");
 		// 2.請印出 CP 最高的景點
 		Attraction best = attractions.stream()
-									 .max((a1, a2) -> (int)(a1.getCp()*1000 - a2.getCp()*1000))
+									 //.max((a1, a2) -> (int)(a1.getCp()*1000 - a2.getCp()*1000))
+									 //.max((a1, a2) -> Double.compare(a1.getCp(), a2.getCp()))
+									 //.max(Comparator.comparingDouble(a -> a.getCp()))
+									 .max(Comparator.comparingDouble(Attraction::getCp))
 									 .orElse(null);
 		System.out.printf("CP 最高的景點: %s%n", best);
 		// 3.請印出 CP 最低的景點
@@ -31,6 +35,9 @@ public class TaichungMain {
 									 .min((a1, a2) -> (int)(a1.getCp()*1000 - a2.getCp()*1000))
 									 .orElse(null);
 		System.out.printf("CP 最低的景點: %s%n", low);
+		
+		
+		
 	}
 
 }
