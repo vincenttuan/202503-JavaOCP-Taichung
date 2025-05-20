2d6d7eace837cbc51f10b5705dbc2feab87d3fcd
FP <FP@FP999>
2025-05-20 18:38:51 +0800

    j

---
+++ JavaOCP/src/day18/TaichungMain.java
@@ -0,0 +1,10 @@
+package day18;
+
+public class TaichungMain {
+
+	public static void main(String[] args) throws Exception {
+		String content = ReadFile.read("src/day18/taichung.txt");
+		System.out.println(content);
+	}
+
+}
