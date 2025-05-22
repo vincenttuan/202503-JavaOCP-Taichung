a0be99e65fd9c858301ab52a5cac7eb6504b0bda
FP <FP@FP999>
2025-05-22 18:54:24 +0800

    j

--- JavaOCP/src/day19/ExceptionDemo1.java
+++ JavaOCP/src/day19/ExceptionDemo1.java
@@ -4,12 +4,23 @@ public class ExceptionDemo1 {
 
 	public static void main(String[] args) {
 		div(10, 2);
+		div(10, 5);
+		div(10, null);
 	}
 	
 	// 除法
 	private static void div(Integer x, Integer y) {
-		Integer result = x / y;
-		System.out.println(result);
+		try {
+			Integer result = x / y;
+			System.out.println(result);
+		} catch (NullPointerException e) {
+			System.out.println("發生空值錯誤");
+			System.out.println("錯誤訊息:" + e.getMessage());
+			System.out.println("錯誤物件:" + e);
+			System.out.println("完整錯誤訊息(如下):");
+			e.printStackTrace(System.out); // 完整錯誤訊息
+		}
+		
 	}
 
 }
