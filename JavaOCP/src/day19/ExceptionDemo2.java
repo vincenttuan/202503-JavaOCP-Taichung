571c56b176523c60ce3e5f36e9572d5da8cc3173
FP <FP@FP999>
2025-05-22 19:05:18 +0800

    j

--- JavaOCP/src/day19/ExceptionDemo2.java
+++ JavaOCP/src/day19/ExceptionDemo2.java
@@ -11,6 +11,14 @@ public class ExceptionDemo2 {
 			System.out.println("請輸入不為 0 的數字");
 		}
 		
+		try {
+			div(10, 0);
+		} catch (NullPointerException e) {
+			System.out.println("Please input a number");
+		} catch (ArithmeticException e) {
+			System.out.println("Please input a number not equals 0");
+		}
+		
 		System.out.println("看到我表示程式正常結束");
 	}
 	
