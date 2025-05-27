dc1c1d413c18b1a138e2e4d9a76d07fd45743e5a
FP <FP@FP999>
2025-05-27 19:35:59 +0800

    j

--- JavaOCP/src/day20/SnackTest2.java
+++ JavaOCP/src/day20/SnackTest2.java
@@ -4,14 +4,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import javax.swing.JOptionPane;
 
 import com.google.gson.Gson;
 
 public class SnackTest2 {
-
+	
+	// 洗牌方法
+	private static void shuffle(Snack[] snacks) {
+		Random random = new Random();
+		for(int i=0;i<1000;i++) {
+			int j = random.nextInt(snacks.length); // 0~29, 因為 snacks.length = 30
+			int k = random.nextInt(snacks.length); // 0~29, 因為 snacks.length = 30
+			Snack tmp = snacks[j];
+			snacks[j] = snacks[k];
+			snacks[k] = tmp;
+		}
+	}
+	
 	public static void main(String[] args) {
 		// -- 定義資料來源
 		Path path = Path.of("src/day20/snacks.json");
@@ -20,6 +34,8 @@ public class SnackTest2 {
 			String json = Files.readString(path);
 			Gson gson = new Gson();
 			Snack[] snacks = gson.fromJson(json, Snack[].class);
+			//-- 洗牌 -----------------------------------------
+			shuffle(snacks);
 			//-- 分析資料 -----------------------------------------
 			// 詢問口袋有多少錢?給予適當的小吃店
 			int balance = Integer.parseInt(JOptionPane.showInputDialog("請問你口袋有多少錢?"));
