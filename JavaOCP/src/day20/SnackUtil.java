d972f46369a76a6d8666609c2f8fd201a69e326f
FP <FP@FP999>
2025-05-27 21:16:14 +0800

    j

--- JavaOCP/src/day20/SnackUtil.java
+++ JavaOCP/src/day20/SnackUtil.java
@@ -1,22 +1,16 @@
 package day20;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.swing.JOptionPane;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 // 工具程式
 public class SnackUtil {
