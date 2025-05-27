e9ba9921e1aaf15ff1aa73865501622a65de1660
FP <FP@FP999>
2025-05-27 20:02:21 +0800

    j

---
+++ JavaOCP/src/day20/HaversineTest.java
@@ -0,0 +1,12 @@
+package day20;
+
+public class HaversineTest {
+
+	public static void main(String[] args) {
+		// 巨匠逢甲分校經緯度:(緯度)24.1761, (經度)120.6402
+		// 大甲芋頭牛奶-文華路入口附近:(緯度)24.1788, (經度)120.6466
+		
+
+	}
+
+}
