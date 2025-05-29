187dac6075d07e32ced21844861ed37c0d1cde1b
FP <FP@FP999>
2025-05-29 19:31:32 +0800

    j

--- JavaOCP/src/day21/Car.java
+++ JavaOCP/src/day21/Car.java
@@ -11,7 +11,10 @@ public class Car {
 	
 	// name: 接收賽車名稱作為參數，並在建構方法內呼叫 CarUtil 的靜態方法初始化 車款、馬力、加速劑。
 	public Car(String name) {
-		
+		this.name = name;
+		this.model = CarUtil.getRandomModel();
+		this.horsepower = CarUtil.getRandomHorsePower();
+		this.boost = CarUtil.getRandomBoost();
 	}
 	
 	public int getFinalHorsePower() {
