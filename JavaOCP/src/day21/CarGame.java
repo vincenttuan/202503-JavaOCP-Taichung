5eff1a17156a734dcd347011c2b40fd319594266
FP <FP@FP999>
2025-05-29 19:43:20 +0800

    j

--- JavaOCP/src/day21/CarGame.java
+++ JavaOCP/src/day21/CarGame.java
@@ -13,16 +13,17 @@ public class CarGame {
 		while (true) {
 			car1 = new Car("Car1");
 			car2 = new Car("Car2");
-			//break;
+			break;
 			/*
 			if(car1.getFinalHorsePower() == car2.getFinalHorsePower()) {
 				break;
 			}
 			*/
+			/*
 			if(car1.getFinalHorsePower() == car2.getFinalHorsePower() && car1.getModel().equals(car2.getModel())) {
 				break;
 			}
-			
+			*/
 		}
 		System.out.printf("參賽名稱:%s、車款:%s、最終馬力:%d%n", 
 				car1.getName(), car1.getModel(), car1.getFinalHorsePower());
