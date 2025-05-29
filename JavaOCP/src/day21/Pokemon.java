bd90e49b58fe46893bce17c767433f9a997607c8
FP <FP@FP999>
2025-05-29 20:02:09 +0800

    j

--- JavaOCP/src/day21/Pokemon.java
+++ JavaOCP/src/day21/Pokemon.java
@@ -20,4 +20,20 @@ public class Pokemon {
 	public void displayInfo() {
 		System.out.printf("名稱:%s 屬性:%s 等級:%d%n", name, type, level);
 	}
+	
+	public static int getPokemonCount() {
+		return pokemonCount;
+	}
+	
+	// evolve()：當寶可夢進化時，如果糖果數量大於或等於 10 顆，
+	// 則將等級增加 1，並減少 10 顆糖果；如果糖果不足，則顯示提示訊息。
+	public void evolve() {
+		if(candy < 10) {
+			System.out.println("進化失敗");
+			return;
+		}
+		level++;
+		candy -= 10;
+		System.out.println("進化成功");
+	}
 }
