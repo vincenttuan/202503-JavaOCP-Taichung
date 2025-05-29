c93a62f04aa29f72a361c357cc7c614b291d87ed
FP <FP@FP999>
2025-05-29 19:10:16 +0800

    j

---
+++ JavaOCP/src/day21/CarUtil.java
@@ -0,0 +1,11 @@
+package day21;
+
+/**
+ * 提供以下靜態方法，隨機生成賽車屬性：
+✓ getRandomModel()：隨機回傳 "Sports Car"、 "Sedan" 或 "SUV"。
+✓ getRandomHorsePower()：隨機生成 150~200 之間的馬力。
+✓ getRandomBoost()：隨機生成 0~3
+ * */
+public class CarUtil {
+	
+}
