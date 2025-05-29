544ff2fbed646b40e096cd581b60f9437e2c9f74
FP <FP@FP999>
2025-05-29 19:33:29 +0800

    j

--- JavaOCP/src/day21/CarGame.java
+++ JavaOCP/src/day21/CarGame.java
@@ -1,5 +1,13 @@
 package day21;
 
+/**
+ * 使用者輸入兩輛賽車的名稱。
+ * 程式自動生成這兩台賽車的車款、馬力與加速劑數量。
+ * 使用 Track 進行比賽，並根據規則決定勝負。
+ * 印出比賽結束後的勝者名稱、車款與最終馬力。
+ * */
 public class CarGame {
-
+	public static void main(String[] args) {
+		
+	}
 }
