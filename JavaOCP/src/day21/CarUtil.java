4ae520b6ce6c5d9c87397f13080dd5b0417ecc9a
FP <FP@FP999>
2025-05-29 19:14:44 +0800

    j

--- JavaOCP/src/day21/CarUtil.java
+++ JavaOCP/src/day21/CarUtil.java
@@ -1,5 +1,7 @@
 package day21;
 
+import java.util.Random;
+
 /**
  * 提供以下靜態方法，隨機生成賽車屬性：
 ✓ getRandomModel()：隨機回傳 "Sports Car"、 "Sedan" 或 "SUV"。
@@ -7,5 +9,20 @@ package day21;
 ✓ getRandomBoost()：隨機生成 0~3
  * */
 public class CarUtil {
+	public static String getRandomModel() {
+		Random random = new Random();
+		String[] models = {"Sports Car", "Sedan", "SUV"};
+		int idx = random.nextInt(models.length); // 0~2
+		return models[idx];
+	}
 	
+	public static int getRandomHorsePower() {
+		Random random = new Random();
+		return random.nextInt(51) + 150;
+	}
+	
+	public static int getRandomBoost() {
+		Random random = new Random();
+		return random.nextInt(4);
+	}
 }
