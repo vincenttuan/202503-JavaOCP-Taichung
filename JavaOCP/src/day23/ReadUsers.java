906b59ec5934b81e14539e2de3d64952901ad97f
FP <FP@FP999>
2025-06-05 19:21:43 +0800

    j

--- JavaOCP/src/day23/ReadUsers.java
+++ JavaOCP/src/day23/ReadUsers.java
@@ -1,11 +1,33 @@
 package day23;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 // 查詢 users 資料表中的資訊
 public class ReadUsers {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ClassNotFoundException {
+		// 利用 Class.forName(xxxx) 註冊 Driver <-- JDBC Type 4 以前要加入
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection conn = null;
+		Statement  stmt = null;
+		ResultSet  rs   = null;
+		
+		String url = "jdbc:mysql://localhost:3306/mydb";
+		String username = "root";
+		String password = "12345678";
+		
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
