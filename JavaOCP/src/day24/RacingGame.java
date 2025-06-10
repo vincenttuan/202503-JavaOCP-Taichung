cf2df457237cf6ec320c75e89f43361c9e403bdf
FP <FP@FP999>
2025-06-10 19:39:39 +0800

    j

---
+++ JavaOCP/src/day24/RacingGame.java
@@ -0,0 +1,20 @@
+package day24;
+
+public class RacingGame {
+
+	public static void main(String[] args) {
+		Horse horse1 = new Horse();
+		Horse horse2 = new Horse();
+		
+		Thread t1 = new Thread(horse1, "白馬"); // 讓 t1 執行緒去執行 horse1 的工作(就是 run() 方法)
+		Thread t2 = new Thread(horse2, "黑馬"); // 讓 t2 執行緒去執行 horse2 的工作(就是 run() 方法)
+		
+		System.out.println("比賽開始");
+		
+		t1.start();
+		t2.start();
+		
+		System.out.println("比賽結束");
+	}
+
+}
