1eaa265d534b4635a60e1e936aaa72bca737deba
FP <FP@FP999>
2025-06-10 19:31:54 +0800

    java

---
+++ JavaOCP/src/day24/Horse.java
@@ -0,0 +1,14 @@
+package day24;
+
+// 賽馬
+public class Horse implements Runnable {
+
+	@Override
+	public void run() {
+		String horseName = Thread.currentThread().getName(); // 目前正在執行的執行緒名
+		for(int i=1;i<=1000;i++) {
+			System.out.printf("%s 跑了 %d 步%n", horseName, i);
+		}
+	}
+	
+}
