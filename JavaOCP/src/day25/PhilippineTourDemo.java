8be988fbc688d4d82bd0da29f3aadc29045dd93a
FP <FP@FP999>
2025-06-12 21:09:05 +0800

    j

---
+++ JavaOCP/src/day25/PhilippineTourDemo.java
@@ -0,0 +1,13 @@
+package day25;
+
+public class PhilippineTourDemo {
+
+	public static void main(String[] args) {
+		PhilippineTour tour = new PhilippineTour(3); // 3 個名額
+		
+		// 模擬 5 個學生去搶
+		
+
+	}
+
+}
