e900109ee3c57e0e6436a457c85488471e44027d
FP <FP@FP999>
2025-06-12 19:14:24 +0800

    j

--- JavaOCP/src/day25/Account.java
+++ JavaOCP/src/day25/Account.java
@@ -4,5 +4,27 @@ public class Account {
 	
 	private Integer balance;
 	
+	public Account(int balance) {
+		this.balance = balance;
+	}
+	
+	public void widthdraw(int amount) {
+		String threadName = Thread.currentThread().getName();
+		System.out.printf("%s 開始提款%n", threadName);
+		// 取得餘額
+		int myBalance = balance;
+		// 檢查餘額是否足夠 ?
+		if(myBalance < amount) {
+			System.out.printf("%s 要提款:%d 失敗 目前餘額: %d 餘額不足%n",
+					threadName, amount, myBalance);
+			return;
+		}
+		// 進行提款
+		myBalance = myBalance - amount; // 扣抵提款金額
+		balance = myBalance; // 將 myBalance 設定給 balance
+		System.out.printf("%s 要提款:%d 成功 目前餘額: %d%n",
+				threadName, amount, myBalance);
+		
+	}
 	
 }
