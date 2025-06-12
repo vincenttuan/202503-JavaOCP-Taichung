dfd5c2aa5c27efe9e4a4af01e4878ea5e1245f72
FP <FP@FP999>
2025-06-12 19:49:18 +0800

    j

--- JavaOCP/src/day25/TicketSystem.java
+++ JavaOCP/src/day25/TicketSystem.java
@@ -7,14 +7,14 @@ public class TicketSystem {
 		return tickets > 0;
 	}
 	
-	public boolean grabTicket(String user) {
+	public synchronized boolean grabTicket(String user) {
 		if(tickets > 0) {
 			System.out.printf("%s 搶到一張票!", user);
 			tickets--;
 			System.out.printf("剩餘票數:%d 張%n", tickets);
 			return true;
 		} else {
-			System.out.printf("%s 沒搶到票, 票已售罄!%n");
+			System.out.printf("%s 沒搶到票, 票已售罄!%n", user);
 			return false;
 		}
 	}
