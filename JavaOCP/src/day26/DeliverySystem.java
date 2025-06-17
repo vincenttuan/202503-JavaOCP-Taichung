e9448c366b97743c77d66cf0382d24a6511f91f9
FP <FP@FP999>
2025-06-17 19:21:44 +0800

    j

--- JavaOCP/src/day26/DeliverySystem.java
+++ JavaOCP/src/day26/DeliverySystem.java
@@ -1,8 +1,6 @@
 package day26;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
