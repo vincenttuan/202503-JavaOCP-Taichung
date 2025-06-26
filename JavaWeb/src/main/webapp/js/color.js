0b4fbaef390a052609f699d37209199e7b89969e
FP <FP@FP999>
2025-06-26 19:51:43 +0800

    web

--- JavaWeb/src/main/webapp/js/color.js
+++ JavaWeb/src/main/webapp/js/color.js
@@ -6,10 +6,21 @@ function checkResult() {
 	const q4 = document.quertSelector('input[name="q4"]:checked');
 	const q5 = document.quertSelector('input[name="q5"]:checked');
 	
+	// 檢查是否每題都有選擇
 	if(!(q1&&q2&&q3&&q4&&q5)) {
 		alert('請完成所有題目!');
 		return;
 	}
+	
+	// 計分
+	const scores = {'紅色':0, '藍色': 0, '綠色':0, '黃色':0};
+	const colorMap = {'A':'紅色', 'B':'藍色', 'C':'綠色', 'D':'黃色'};
+	
+	[q1, q2, q3, q4, q5].forEach(q => {
+		scores[colorMap[q.value]] += 1;
+	});
+	
+	alert(scores);
 		
 	
 }
