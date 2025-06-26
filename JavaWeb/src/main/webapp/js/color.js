8689b73b1206dac2d614a016b0a3974767ed3aa9
FP <FP@FP999>
2025-06-26 19:43:40 +0800

    web

--- JavaWeb/src/main/webapp/js/color.js
+++ JavaWeb/src/main/webapp/js/color.js
@@ -1,3 +1,15 @@
-/**
- * 
- */
+function checkResult() {
+	// 取得每一題的選項值
+	const q1 = document.quertSelector('input[name="q1"]:checked');
+	const q2 = document.quertSelector('input[name="q2"]:checked');
+	const q3 = document.quertSelector('input[name="q3"]:checked');
+	const q4 = document.quertSelector('input[name="q4"]:checked');
+	const q5 = document.quertSelector('input[name="q5"]:checked');
+	
+	if(!(q1&&q2&&q3&&q4&&q5)) {
+		alert('請完成所有題目!');
+		return;
+	}
+		
+	
+}
