74c3e81e87044d8d14b861296d80f16265734b4a
FP <FP@FP999>
2025-07-08 19:53:56 +0800

    web

--- JavaWeb/src/main/java/servlet/GetSessionIdServlet.java
+++ JavaWeb/src/main/java/servlet/GetSessionIdServlet.java
@@ -15,6 +15,17 @@ public class GetSessionIdServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html;chatset=utf-8");
+		
+		// username=admin&password=1234 才能拿到 session
+		String username = req.getParameter("username") + "";
+		String password = req.getParameter("password") + "";
+		
+		// 判斷
+		if (!(username.equals("admin") && password.equals("1234"))) {
+			resp.getWriter().print("帳密錯誤!");
+			return;
+		}
+		
 		// 取得 HttpSession
 		HttpSession session = req.getSession();
 		if(session.isNew()) {
