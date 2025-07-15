29764c6b918b4e1a9e2371d8aaccbf72007a10c9
FP <FP@FP999>
2025-07-15 21:06:30 +0800

    web

---
+++ JavaWeb/src/main/java/dao/UserDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.util.List;
+
+import model.User;
+
+public interface UserDao {
+	
+	List<User> findAllUsers();
+	User getUserById(Integer id);
+	User getUserByUsername(String username);
+	void addUser(String username, String hash, String salt);
+	void changePasswordById(Integer id, String newPassword);
+	void deleteUserById(Integer id);
+	
+}
