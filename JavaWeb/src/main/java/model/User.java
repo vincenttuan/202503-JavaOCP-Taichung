fe9ca88ea706ce616711bb0d2d8a77d08f8fe5b2
FP <FP@FP999>
2025-07-15 21:00:31 +0800

    web

--- JavaWeb/src/main/java/model/User.java
+++ JavaWeb/src/main/java/model/User.java
@@ -3,8 +3,18 @@ package model;
 import lombok.Data;
 
 // 使用者資料
+/**
+use web;
+create table if not exists user (
+	id int auto_increment primary key,
+    username varchar(50) not null unique,
+    hash varchar(255) not null,
+    salt varchar(255) not null
+);
+ */
 @Data
 public class User {
+	private Integer id;
 	private String username;
 	private String hash;
 	private String salt;
