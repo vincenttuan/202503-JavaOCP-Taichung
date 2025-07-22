bacf79e1c9993fed9951e9b3546fb6e9a9722762
FP <FP@FP999>
2025-07-22 20:39:44 +0800

    web

---
+++ JavaWeb/src/main/java/service/UserService.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.util.List;
+
+import model.User;
+
+public interface UserService {
+	List<User> findAllUsers();
+	User getUserById(Integer id);
+	User getUserByUsername(String username);
+	void addUser(String username, String password);
+	void changePasswordById(Integer id, String oldPassword, String newPassword);
+	void deleteUserById(Integer id);
+}
