8ca55f6ba9fd7c30e742442e5bebf6260f9c7cc3
FP <FP@FP999>
2025-07-31 21:19:42 +0800

    web

--- JavaWeb/src/main/java/filter/LoginFilter.java
+++ JavaWeb/src/main/java/filter/LoginFilter.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpSession;
 
 // 有登入才放行的過濾器
 //@WebFilter(urlPatterns = {"/user", "/user/delete", "/user/change/password"})
-@WebFilter(urlPatterns = {"/user/*", "/beverage", "/japanese/class/level"})
+@WebFilter(urlPatterns = {"/user/*", "/beverage", "/japanese/class/level", "/chart"})
 public class LoginFilter extends HttpFilter {
 	@Override
 	protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain)
