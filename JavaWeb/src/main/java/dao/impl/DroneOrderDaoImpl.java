141833431c6de825ed6f80de2c8d3196459609f7
FP <FP@FP999>
2025-08-05 19:18:31 +0800

    web

--- JavaWeb/src/main/java/dao/impl/DroneOrderDaoImpl.java
+++ JavaWeb/src/main/java/dao/impl/DroneOrderDaoImpl.java
@@ -1,5 +1,9 @@
 package dao.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import dao.BaseDao;
@@ -10,8 +14,38 @@ public class DroneOrderDaoImpl extends BaseDao implements DroneOrderDao {
 
 	@Override
 	public List<DroneOrder> findAll(String orderByName, boolean asc) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = """
+				select order_id, customer_name, drone_model, height, speed, quantity, price, order_date, status 
+				from drone_order
+				order by %s %s
+				""";
+		sql = String.format(sql.trim(), orderByName, asc?"asc":"desc");
+		List<DroneOrder> droneOrders = new ArrayList<>();
+		
+		try(Statement stmt = getConnection().createStatement();
+			ResultSet rs = stmt.executeQuery(sql)) {
+			
+			while (rs.next()) {
+				DroneOrder droneOrder = new DroneOrder();
+				droneOrder.setOrderId(rs.getInt("order_id"));
+				droneOrder.setCustomerName(rs.getString("customer_name"));
+				droneOrder.setDroneModel(rs.getString("drone_model"));
+				droneOrder.setHeight(rs.getInt("height"));
+				droneOrder.setSpeed(rs.getInt("speed"));
+				droneOrder.setQuantity(rs.getInt("quantity"));
+				droneOrder.setPrice(rs.getInt("price"));
+				droneOrder.setOrderDate(rs.getDate("order_date"));
+				droneOrder.setStatus(rs.getString("status"));
+				// 注入到集合中保存
+				droneOrders.add(droneOrder);
+			}
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("查詢失敗: " + e);
+		}
+		
+		
+		return droneOrders;
 	}
 
 	@Override
