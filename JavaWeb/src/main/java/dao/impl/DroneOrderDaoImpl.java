606b1c08808f75b48807b6035e7c302c3eee239b
FP <FP@FP999>
2025-08-05 19:03:31 +0800

    web

---
+++ JavaWeb/src/main/java/dao/impl/DroneOrderDaoImpl.java
@@ -0,0 +1,22 @@
+package dao.impl;
+
+import java.util.List;
+
+import dao.BaseDao;
+import dao.DroneOrderDao;
+
+public class DroneOrderDaoImpl extends BaseDao implements DroneOrderDao {
+
+	@Override
+	public List<DroneOrderDao> findAll(String orderByName, boolean asc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DroneOrderDao> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
