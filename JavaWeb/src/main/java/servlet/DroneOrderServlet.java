1b9d10a85739354d4cfe49602d913de5538940dc
FP <FP@FP999>
2025-08-05 19:55:41 +0800

    web

---
+++ JavaWeb/src/main/java/servlet/DroneOrderServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.DroneOrder;
+import service.DroneOrderService;
+import service.impl.DroneOrderServiceImpl;
+
+@WebServlet("/drone/order")
+public class DroneOrderServlet extends HttpServlet {
+	
+	private DroneOrderService service = new DroneOrderServiceImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String orderByName = req.getParameter("orderByName");
+		String asc = req.getParameter("asc");
+		// 取得所有無人機訂單資料
+		if(orderByName == null) {
+			orderByName = "order_id";
+		}
+		asc = (asc == null) ? "true" : asc;
+		
+		List<DroneOrder> droneOrders = service.findAll(orderByName, Boolean.parseBoolean(asc));
+		req.setAttribute("droneOrders", droneOrders);
+		// 重導 /WEB-INF/view/drone_order.jsp
+		req.getRequestDispatcher("/WEB-INF/view/drone_order.jsp").forward(req, resp);
+	}
+	
+}
