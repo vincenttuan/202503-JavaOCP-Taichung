17737957f7ced939defd72aa436fd1a83e335015
FP <FP@FP999>
2025-08-14 19:07:33 +0800

    web

--- JavaWeb/src/main/java/websocket/ChatServer.java
+++ JavaWeb/src/main/java/websocket/ChatServer.java
@@ -15,7 +15,9 @@ public class ChatServer {
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 已連入%n", sessionId);
 		String message = String.format("%s 已進入聊天室%n", sessionId);
-		session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		if(session.isOpen()) {
+			session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		}
 	}
 	
 	@OnMessage
@@ -23,7 +25,9 @@ public class ChatServer {
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 說: %s%n", sessionId, message);
 		message = String.format("%s 說: %s%n", sessionId, message);
-		session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		if(session.isOpen()) {
+			session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		}
 	}
 	
 	@OnClose
@@ -31,7 +35,9 @@ public class ChatServer {
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 已關閉%n", sessionId);
 		String message = String.format("%s 已離開聊天室%n", sessionId);
-		session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		if(session.isOpen()) {
+			session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		}
 	}
 	
 	@OnError
