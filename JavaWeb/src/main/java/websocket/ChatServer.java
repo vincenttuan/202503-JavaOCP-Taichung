1b68ab82d9ca6ab7384c4e7991690b9ff96facbe
FP <FP@FP999>
2025-08-14 18:41:36 +0800

    web

--- JavaWeb/src/main/java/websocket/ChatServer.java
+++ JavaWeb/src/main/java/websocket/ChatServer.java
@@ -12,22 +12,26 @@ public class ChatServer {
 	
 	@OnOpen
 	public void onOpen(Session session) {
-		
+		String sessionId = session.getId();
+		System.out.printf("session id: %s 已連入%n", sessionId);
 	}
 	
 	@OnMessage
 	public void onMessage(Session session, String message) {
-		
+		String sessionId = session.getId();
+		System.out.printf("session id: %s 說: %s%n", sessionId, message);
 	}
 	
 	@OnClose
 	public void onClose(Session session) {
-		
+		String sessionId = session.getId();
+		System.out.printf("session id: %s 已關閉%n", sessionId);
 	}
 	
 	@OnError
 	public void onError(Session session, Throwable throwable) {
-		
+		String sessionId = session.getId();
+		System.out.printf("session id: %s 發生錯誤: %s%n", sessionId, throwable);
 	}
 	
 }
