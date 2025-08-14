41f67898cf03369c226d08c6b8daa9727c745c41
FP <FP@FP999>
2025-08-14 21:08:48 +0800

    web

--- JavaWeb/src/main/java/websocket/ChatServer.java
+++ JavaWeb/src/main/java/websocket/ChatServer.java
@@ -3,6 +3,7 @@ package websocket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import jakarta.servlet.http.HttpSession;
@@ -14,18 +15,24 @@ import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
+class ChatUser {
+	Session session;
+	String username;
+}
+
 @ServerEndpoint(value = "/chatserver", configurator = GetHttpSessionConfig.class)
 public class ChatServer {
+	
 	// 建立一個 List<Session> 存放所有的連線資訊
-	private static List<Session> sessions = new CopyOnWriteArrayList<>();
+	private static List<ChatUser> chatUsers = new CopyOnWriteArrayList<>();
 	
 	// 廣播
 	private void broadcast(String sessionId, String message) {
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		message = String.format("%8s %s 說 %s", sdf.format(new Date()), sessionId, message);
-		for(Session session : sessions) {
-			if(session.isOpen()) {
-				session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		for(ChatUser chatUser : chatUsers) {
+			if(chatUser.session.isOpen()) {
+				message = String.format("%8s %s 說 %s", sdf.format(new Date()), chatUser.username, message);
+				chatUser.session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
 			}
 		}
 	}
@@ -34,8 +41,12 @@ public class ChatServer {
 	public void onOpen(Session session, EndpointConfig config) {
 		HttpSession httpSession = (HttpSession)config.getUserProperties().get(HttpSession.class.getName());
 		String username = httpSession.getAttribute("username") + "";
+		ChatUser chatUser = new ChatUser();
+		chatUser.session = session;
+		chatUser.username = username;
 		
-		sessions.add(session);
+		chatUsers.add(chatUser);
+		
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 已連入%n", sessionId);
 		String message = String.format("%s 已進入聊天室%n", username);
@@ -43,25 +54,20 @@ public class ChatServer {
 	}
 	
 	@OnMessage
-	public void onMessage(Session session, String message, EndpointConfig config) {
-		HttpSession httpSession = (HttpSession)config.getUserProperties().get(HttpSession.class.getName());
-		String username = httpSession.getAttribute("username") + "";
-		
+	public void onMessage(Session session, String message) {
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 說: %s%n", sessionId, message);
-		message = String.format("%s 說: %s%n", username, message);
+		message = String.format("%s%n", message);
 		broadcast(sessionId, message);
 	}
 	
 	@OnClose
-	public void onClose(Session session, EndpointConfig config) {
-		HttpSession httpSession = (HttpSession)config.getUserProperties().get(HttpSession.class.getName());
-		String username = httpSession.getAttribute("username") + "";
+	public void onClose(Session session) {
+		chatUsers.removeIf(chatUser -> chatUser.session.equals(session));
 		
-		sessions.remove(session);
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 已關閉%n", sessionId);
-		String message = String.format("%s 已離開聊天室%n", username);
+		String message = String.format("%s 已離開聊天室%n", sessionId);
 		broadcast(sessionId, message);
 	}
 	
