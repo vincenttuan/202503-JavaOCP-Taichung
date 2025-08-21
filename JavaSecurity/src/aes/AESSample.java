385348476357a4fc6707f4c9023c10ba4e3d2a38
FP <FP@FP999>
2025-08-21 20:04:55 +0800

    web

--- JavaSecurity/src/aes/AESSample.java
+++ JavaSecurity/src/aes/AESSample.java
@@ -9,7 +9,8 @@ import util.KeyUtil;
 public class AESSample {
 	
 	// 建立一個 AES Key (256bits, 32bytes)
-	private static final String KEY = "012345678901234567890123456789AB"; // 32個字
+	private static final String KEY = "012345678901234567890123456789AB";  // 32個字
+	private static final String KEY2 = "012345678901234567890123456789AB"; // 32個字
 	
 	public static void main(String[] args) throws Exception {
 		// 明文
@@ -19,13 +20,14 @@ public class AESSample {
 		// 利用 AES 進行加密
 		// 1. 建立密鑰(AES的密鑰)
 		SecretKeySpec myKey = new SecretKeySpec(KEY.getBytes(), "AES");
+		SecretKeySpec bossKey = new SecretKeySpec(KEY2.getBytes(), "AES");
 		// 2. 將明文加密
 		byte[] encrypted = KeyUtil.encryptWithAESKey(myKey, originalText);
 		// 3.印出密文
 		System.out.printf("密文: %s%n", Arrays.toString(encrypted));
 		System.out.println("-------------------------");
 		// 4. 解密
-		String output = KeyUtil.decryptWithAESKey(myKey, encrypted); // 拿到密文去解密
+		String output = KeyUtil.decryptWithAESKey(bossKey, encrypted); // 拿到密文去解密
 		System.out.printf("解密: %s%n", output);
 	}
 	
