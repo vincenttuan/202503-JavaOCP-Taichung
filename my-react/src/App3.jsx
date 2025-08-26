998aa1c5af31f9048c6bc7973ab34e30aac5d458
FP <FP@FP999>
2025-08-26 19:43:30 +0800

    web

--- my-react/src/App3.jsx
+++ my-react/src/App3.jsx
@@ -4,9 +4,23 @@ function App() {
         alert('按我一下');
     }
 
+    const touchMe = () => {
+        alert('碰到我了')
+    }
+
+    function handleMouseDown(event) {
+        if(event.button == 0) {
+            alert('你按了左鍵');
+        } else if(event.button == 2) {
+            alert('你按了右鍵');
+        }
+    }
+
     return(
         <>
             <button onClick={clickMe}>好好按我</button>
+            <button onClick={touchMe}>不要碰我</button>
+            <button onMouseDown={handleMouseDown}>判斷按鍵</button>
         </>
     )
 }
