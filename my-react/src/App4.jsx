764325496dd6c33d15e77dace3287dec21a50ebf
FP <FP@FP999>
2025-08-26 20:32:22 +0800

    react

--- my-react/src/App4.jsx
+++ my-react/src/App4.jsx
@@ -3,14 +3,14 @@ function App() {
     const fruits = ['西瓜', '芒果', '荔枝', '龍眼', '椰子'];
     //                0       1       2      3       4
     const fruits2 = [
-        <li>西瓜</li>,
-        <li>芒果</li>,
-        <li>荔枝</li>,
-        <li>龍眼</li>,
-        <li>椰子</li>
+        <li key="0">西瓜</li>,
+        <li key="1">芒果</li>,
+        <li key="2">荔枝</li>,
+        <li key="3">龍眼</li>,
+        <li key="4">椰子</li>
     ];
 
-    const fruits3 = fruits.map((fruit, index) => (<li>{index} : {fruit}</li>))
+    const fruits3 = fruits.map((fruit, index) => (<li key={index}>{index} : {fruit}</li>))
 
     return(
         <>
