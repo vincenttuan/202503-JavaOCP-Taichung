c797ae876c6950ab71c7641fa260396cf2c908be
FP <FP@FP999>
2025-08-28 19:58:13 +0800

    react

---
+++ my-react/src/App10.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    function clickMe() {
+        setCount(count + 1);
+    }
+
+    return(
+        <>
+            <div style={{fontSize: 50}}>{count}</div>
+            <button onClick={clickMe}>按我一下可以 + 1</button>
+        </>
+    )
+}
+
+export default App;
