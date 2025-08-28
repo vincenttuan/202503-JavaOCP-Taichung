a0ff5d21b443a5eb17ce49d524bd1905e2153cec
FP <FP@FP999>
2025-08-28 19:32:03 +0800

    web

---
+++ my-react/src/App9.jsx
@@ -0,0 +1,14 @@
+// 傳統資料渲染與更新
+function App() {
+    var count = 0;
+
+    return(
+        <>
+            <div style={{fontSize: 50}}>{count}</div>
+            <button>按我一下可以 + 1</button>
+        </>
+    )
+
+}
+
+export default App;
