39e80358f400cfb99557c3ce95edfa239b80a1fd
FP <FP@FP999>
2025-08-28 19:37:26 +0800

    web

--- my-react/src/App9.jsx
+++ my-react/src/App9.jsx
@@ -2,10 +2,15 @@
 function App() {
     var count = 0;
 
+    function clickMe() {
+        count++; // 更新 count 相當於 setCount()
+        document.getElementById("count").textContent = count; // 資料重新渲染更新
+    }
+
     return(
         <>
-            <div style={{fontSize: 50}}>{count}</div>
-            <button>按我一下可以 + 1</button>
+            <div style={{fontSize: 50}} id="count">0</div>
+            <button onClick={clickMe}>按我一下可以 + 1</button>
         </>
     )
 
