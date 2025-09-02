bf23bc64967a3229747715555e997891ed12bd68
FP <FP@FP999>
2025-09-02 20:56:42 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -2,21 +2,33 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
 	
-	// 執行路徑: http://localhost:8080/api/hello
-	@GetMapping("/api/hello")
+	// 執行路徑: /api/hello
+	@GetMapping("/hello")
 	public String hello() {
 		return "Hello Springboot";
 	}
 	
-	// 執行路徑: http://localhost:8080/api/hi
-	@GetMapping("/api/hi")
+	// 執行路徑: /api/hi
+	@GetMapping("/hi")
 	public String hi() {
 		return "嗨~ Springboot";
 	}
+	
+	// 執行路徑: /api/bmi?h=170&w=60
+	@GetMapping("/bmi")
+	public String bmi(@RequestParam(name = "h") double h, @RequestParam double w) {
+		double bmi = w / Math.pow(h/100, 2);
+		return String.format("身高: %.1f 體重: %.1f BMI: %.2f", h, w, bmi);
+	}
+	
+	
 	
 }
