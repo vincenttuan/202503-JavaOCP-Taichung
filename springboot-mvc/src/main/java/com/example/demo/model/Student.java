c46fbfc2ba38ee87875fdee9f5fd6960b843a1fd
FP <FP@FP999>
2025-09-04 20:53:35 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+	private Integer id;
+	private String name;
+	private Integer age;
+	
+}
