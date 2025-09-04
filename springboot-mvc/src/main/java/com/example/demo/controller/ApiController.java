b3eaa0a7e0d1977daedff097e87f650b14eb3627
FP <FP@FP999>
2025-09-04 18:22:50 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,10 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.SpringbootMvcApplication;
-
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
