84044eaa37d42b0acbfe1f0943cf354b0398dbec
FP <FP@FP999>
2025-09-04 19:25:54 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/demo/model/Sugar.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Sugar {
+	private Integer volume;
+	private Integer sugar;
+	private Integer totalSugar;
+	
+}
