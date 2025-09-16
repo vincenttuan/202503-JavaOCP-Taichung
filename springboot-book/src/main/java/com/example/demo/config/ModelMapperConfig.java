fec422a825d1ca65ad03d1c025ef4082818f2daf
FP <FP@FP999>
2025-09-16 20:16:03 +0800

    web

--- springboot-book/src/main/java/com/example/demo/config/ModelMapperConfig.java
+++ springboot-book/src/main/java/com/example/demo/config/ModelMapperConfig.java
@@ -5,14 +5,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import com.example.demo.model.dto.BookDTO;
+import com.example.demo.model.entity.Book;
+
 @Configuration
 public class ModelMapperConfig {
+
+    private final ModelMapper modelMapper;
+
+    ModelMapperConfig(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 	
 	@Bean
 	//@Scope("singleton") // 每次呼叫都使用同一個物件(預設)
 	//@Scope("prototype") // 每次呼叫都會產生一個新的物件
 	ModelMapper modelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
+		// Entity -> DTO
+		modelMapper.typeMap(Book.class, BookDTO.class).addMappings(mapper -> {
+			
+		});
+		
+		// DTO -> Entity 
+		modelMapper.typeMap(BookDTO.class, Book.class).addMappings(mapper -> {
+			
+		});
 		
 		return modelMapper;
 	}
