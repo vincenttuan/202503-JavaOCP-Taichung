e52cf985b19ff5f48db76c650839271982cd889d
FP <FP@FP999>
2025-09-18 19:58:41 +0800

    web

---
+++ springboot-book/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* CRUD 現代設計風格(Rest):
+* GET    /books   查詢所有書籍 @GetMapping   (value = "/books",     produces = "application/json;charset=utf-8")
+* GET    /book/1  查詢單筆書籍 @GetMapping   (value = "/book/{id}", produces = "application/json;charset=utf-8")
+* POST   /book    新增單筆書籍 @PostMapping  (value = "/book",      produces = "application/json;charset=utf-8")
+* PUT    /book/1  修改單筆書籍 @PutMapping   (value = "/book/{id}", produces = "application/json;charset=utf-8")
+* DELETE /book/1  刪除單筆書籍 @DeleteMapping(value = "/book/{id}", produces = "application/json;charset=utf-8")
+*/
+
+@RestController
+public class BookController {
+	
+	
+	
+	
+	
+}
