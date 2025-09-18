ed39953a10f2adb40ba8c02a9c1bcc73e74b612a
FP <FP@FP999>
2025-09-18 20:48:55 +0800

    web

--- springboot-book/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-book/src/main/java/com/example/demo/controller/BookController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.exception.BookNotFoundException;
 import com.example.demo.model.dto.BookDTO;
 import com.example.demo.response.ApiResponse;
 import com.example.demo.service.BookService;
@@ -27,11 +29,20 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 	
-	
 	@GetMapping(value = "/books", produces = "application/json;charset=utf-8")
 	public ApiResponse<List<BookDTO>> getAllBooks() {
 		List<BookDTO> bookDTOs = bookService.findAllBooks();
 		return new ApiResponse<>(true, bookDTOs, "查詢成功");
+	}
+	
+	@GetMapping(value = "/book/{id}", produces = "application/json;charset=utf-8")
+	public ApiResponse<BookDTO> getBookById(@PathVariable Integer id) {
+		try {
+			BookDTO bookDTO = bookService.findBookById(id);
+			return new ApiResponse<>(true, bookDTO, "查詢成功");
+		} catch (BookNotFoundException e) {
+			return new ApiResponse<>(false, null, e.getMessage());
+		}
 	}
 	
 	
