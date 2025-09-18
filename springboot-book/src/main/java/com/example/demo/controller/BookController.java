cd29f5d78022faa115888c3351c6ff571d9f8a95
FP <FP@FP999>
2025-09-18 19:59:27 +0800

    web

--- springboot-book/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-book/src/main/java/com/example/demo/controller/BookController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.BookService;
 
 /**
 * CRUD 現代設計風格(Rest):
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 	
+	@Autowired
+	private BookService bookService;
 	
 	
 	
