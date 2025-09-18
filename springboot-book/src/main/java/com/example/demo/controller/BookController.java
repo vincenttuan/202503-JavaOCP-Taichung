cc3a2099a04d366a160e0de31477c969c99beb57
FP <FP@FP999>
2025-09-18 20:52:30 +0800

    web

--- springboot-book/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-book/src/main/java/com/example/demo/controller/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,6 +46,13 @@ public class BookController {
 			return new ApiResponse<>(false, null, e.getMessage());
 		}
 	}
+	
+	@PostMapping(value = "/book", produces = "application/json;charset=utf-8")
+	public ApiResponse<BookDTO> addBook(@RequestBody BookDTO bookDTO) {
+		BookDTO createBookDTO = bookService.addBook(bookDTO);
+		return new ApiResponse<>(true, createBookDTO, "單筆新增成功");
+	}
+	
 	
 	
 }
