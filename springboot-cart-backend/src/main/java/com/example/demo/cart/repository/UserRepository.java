5c1f210ca13e974a4374904792d2d8bb5828158d
FP <FP@FP999>
2025-09-30 21:07:33 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/repository/UserRepository.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/repository/UserRepository.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.cart.model.entity.User;
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
-	List<User> findByUsername(String username);
+	List<User> findByUsername(String username); // 屬於多筆查詢
+	
+	Optional<User> findFirstByUsername(String username); // 屬於單筆查詢
 	
 }
