7a0fbc615f50e5d22a94efc413cb4dca6f41cd3e
FP <FP@FP999>
2025-09-30 21:18:52 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/repository/UserRepository.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.cart.model.entity.User;
@@ -15,4 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	
 	Optional<User> findFirstByUsername(String username); // 屬於單筆查詢
 	
+	// 利用原生 sql
+	@Query(value = "select * from user where username = :username", nativeQuery = true)
+	Optional<User> findByUsernameNative(@Param("username") String username);
 }
