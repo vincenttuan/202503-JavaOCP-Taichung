364bc20d3372b0b45ec591a6fe8fbfb2977a2c68
FP <FP@FP999>
2025-09-30 21:02:33 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/repository/UserRepository.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/repository/UserRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.cart.model.entity.User;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	
+	List<User> findByUsername(String username);
+	
 }
