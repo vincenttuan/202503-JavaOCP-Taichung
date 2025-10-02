507835b16271a125883800c1aec5405fb458cc8f
FP <FP@FP999>
2025-10-02 20:09:22 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Product.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Product.java
@@ -1,10 +1,13 @@
 package com.example.demo.cart.model.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,6 +28,8 @@ public class Product {
 	@Column(nullable = false)
 	private Integer price;
 	
-	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "product_image_id")
+	private ProductImage productImage;
 	
 }
