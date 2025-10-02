2009aa2bddb5477f92b6a6df3b908a59399b4b21
FP <FP@FP999>
2025-10-02 20:51:20 +0800

    web

--- springboot-cart-backend/src/test/java/com/example/demo/test/product/AddProduct.java
+++ springboot-cart-backend/src/test/java/com/example/demo/test/product/AddProduct.java
@@ -1,8 +1,20 @@
 package com.example.demo.test.product;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.cart.repository.ProductRepository;
 
 @SpringBootTest
 public class AddProduct {
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	@Test
+	void add() {
+		
+	}
 	
 }
