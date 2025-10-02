880c13b342d80850ae9787f9ce2b8ddb7e11d9dc
FP <FP@FP999>
2025-10-02 19:22:29 +0800

    web

---
+++ springboot-cart-backend/src/test/java/com/example/demo/test/DeleteUser.java
@@ -0,0 +1,23 @@
+package com.example.demo.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.cart.repository.UserRepository;
+
+@SpringBootTest
+public class DeleteUser {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Test
+	void deleteUser() {
+		
+		userRepository.deleteById(2L);
+		System.out.println("刪除");
+		
+	}
+	
+}
