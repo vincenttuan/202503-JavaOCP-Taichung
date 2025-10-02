05c9fa7d03713ad48eabfd61fef85dd4fcc0eb53
FP <FP@FP999>
2025-10-02 19:06:04 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/repository/UserRepository.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.demo.cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.cart.model.entity.User;
+
+import jakarta.transaction.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -20,4 +24,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	// 利用原生 sql
 	@Query(value = "select * from user where username = :username", nativeQuery = true)
 	Optional<User> findByUsernameNative(@Param("username") String username);
+	
+	@Modifying
+	@Transactional
+	@Query(value = "update user set password = :password where id = :id", nativeQuery = true)
+	int updatePasswordByIdNative(@Param("id") Long id, @Param("password") String password);
+	
 }
