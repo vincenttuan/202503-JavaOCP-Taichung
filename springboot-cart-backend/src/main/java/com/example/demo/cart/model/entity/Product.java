d8c20d68f862f28416d5b64fd8b10c26075189df
FP <FP@FP999>
2025-10-07 20:48:01 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Product.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Product.java
@@ -1,6 +1,7 @@
 package com.example.demo.cart.model.entity;
 
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -38,4 +40,8 @@ public class Product {
 	
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<OrderItem> orderItems;
+	
+	@ManyToMany(mappedBy = "favoriteProducts")
+	private Set<User> favoriteUsers;
+	
 }
