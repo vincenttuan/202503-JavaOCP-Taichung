e3e0d737e6f4002e57fb968753af90eec8f54069
FP <FP@FP999>
2025-10-07 18:52:03 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Order.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Order.java
@@ -1,9 +1,12 @@
 package com.example.demo.cart.model.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +21,7 @@ public class Order {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	
+	@OneToMany
+	private List<OrderItem> orderItems;
 	
 }
--- springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/OrderItem.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/OrderItem.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,7 +24,8 @@ public class OrderItem {
 	@Column(nullable = false)
 	private int qty;
 	
-	
+	@ManyToOne
+	private Order order;
 	
 	
 }
