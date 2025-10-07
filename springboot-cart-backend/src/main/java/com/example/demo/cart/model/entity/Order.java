1b16c13f0fb0acea5a5c960ca82830bd521d211c
FP <FP@FP999>
2025-10-07 19:36:17 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Order.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Order.java
@@ -8,6 +8,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -28,5 +30,9 @@ public class Order {
 	// cascade 連動關係
 	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<OrderItem> orderItems;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 	
 }
