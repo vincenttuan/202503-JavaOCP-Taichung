0edaaced084f221595117eaf00db8be9ae47ed2d
FP <FP@FP999>
2025-10-07 19:38:35 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/User.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/User.java
@@ -1,10 +1,15 @@
 package com.example.demo.cart.model.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,4 +33,6 @@ public class User {
 	@Column(length = 255, nullable = false)
 	private String password;
 	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<Order> orders;
 }
