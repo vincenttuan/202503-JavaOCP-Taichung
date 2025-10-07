69067fc616f3b3e56845c52b52223e53de9197fb
FP <FP@FP999>
2025-10-07 18:50:35 +0800

    web

---
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/OrderItem.java
@@ -0,0 +1,29 @@
+package com.example.demo.cart.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	// 商品數量
+	@Column(nullable = false)
+	private int qty;
+	
+	
+	
+	
+}
