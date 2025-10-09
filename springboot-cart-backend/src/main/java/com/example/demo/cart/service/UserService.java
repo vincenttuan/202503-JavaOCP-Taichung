57dbb83ea57558f6ada5735cca8609b1f61d9f95
FP <FP@FP999>
2025-10-09 19:38:26 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/service/UserService.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/service/UserService.java
@@ -18,8 +18,10 @@ public interface UserService {
 	// 用戶關注列表(用戶關注那些商品) - 正向查詢
 	List<FavoriteProductDTO> getFavoriteProducts(Long userId);
 	// 商品關注列表(商品被那些用戶關注) - 反向查詢
-	
+	List<FavoriteUserDTO> getFavoriteUsers(Long productId);
 	// 新增商品關注
-	
+	void addFavoriteProduct(Long userId, Long productId);
 	// 移除所關注的商品
+	void removeFavoriteProduct(Long userId, Long productId);
+	
 }
