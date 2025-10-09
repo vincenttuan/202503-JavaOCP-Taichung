371861a4c9f99cf6f7d98eac2baec6211f16cd05
FP <FP@FP999>
2025-10-09 19:07:26 +0800

    web

---
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/dto/OrderItemDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.cart.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderItemDTO {
+	private Long id;
+	private Integer qty;
+	private ProductDTO productDTO;
+}
