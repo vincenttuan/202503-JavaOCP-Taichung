6e64c52cc83d189805c0ae888ce646ca7f0cec3e
FP <FP@FP999>
2025-10-09 19:11:34 +0800

    cart

---
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/dto/FavorateProductDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.cart.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FavorateProductDTO {
+	private Long id;
+	private String name;
+}
