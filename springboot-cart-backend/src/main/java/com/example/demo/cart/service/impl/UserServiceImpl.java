467680bd5dc023e7a3d94ba815cc42c010c37d11
FP <FP@FP999>
2025-10-09 21:03:22 +0800

    cart

--- springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/UserServiceImpl.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.cart.service.impl;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import com.example.demo.cart.model.dto.FavoriteProductDTO;
 import com.example.demo.cart.model.dto.FavoriteUserDTO;
 import com.example.demo.cart.model.dto.LoginDTO;
 import com.example.demo.cart.model.dto.UserDTO;
+import com.example.demo.cart.model.entity.Product;
 import com.example.demo.cart.model.entity.User;
 import com.example.demo.cart.repository.ProductRepository;
 import com.example.demo.cart.repository.UserRepository;
@@ -80,9 +82,11 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public List<FavoriteProductDTO> getFavoriteProducts(Long userId) {
-		User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("查無使用者"));
-		
+	public List<FavoriteProductDTO> getFavoriteProducts(Long userId) throws UserNotFoundException {
+		User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("查無使用者id:" + userId));
+		// 查詢該用戶所關注的商品
+		Set<Product> products = user.getFavoriteProducts();
+		// 將 products 集合中的每一個元素 Product 一個一個轉 FavoriteProductDTO 最後放到 List 保存
 		return null;
 	}
 
