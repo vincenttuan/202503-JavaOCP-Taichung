42b907add8bfb41bb0df196e959f1f791162f9b0
FP <FP@FP999>
2025-10-09 21:15:08 +0800

    web

--- springboot-cart-backend/src/main/java/com/example/demo/cart/service/UserService.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.example.demo.cart.exception.AddException;
 import com.example.demo.cart.exception.LoginException;
+import com.example.demo.cart.exception.ProductNotFoundException;
 import com.example.demo.cart.exception.UserNotFoundException;
 import com.example.demo.cart.model.dto.FavoriteProductDTO;
 import com.example.demo.cart.model.dto.FavoriteUserDTO;
@@ -20,7 +21,7 @@ public interface UserService {
 	// 用戶關注列表(用戶關注那些商品) - 正向查詢
 	List<FavoriteProductDTO> getFavoriteProducts(Long userId) throws UserNotFoundException;
 	// 商品關注列表(商品被那些用戶關注) - 反向查詢
-	List<FavoriteUserDTO> getFavoriteUsers(Long productId);
+	List<FavoriteUserDTO> getFavoriteUsers(Long productId) throws ProductNotFoundException;
 	// 新增商品關注
 	void addFavoriteProduct(Long userId, Long productId);
 	// 移除所關注的商品
