83ddeefd848073f074d9bf790e4d4968dc074e32
FP <FP@FP999>
2025-10-09 19:13:03 +0800

    cart

---
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/model/dto/LoginDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.cart.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginDTO {
+	private String username;
+	private String password;
+	private Boolean isLoggedIn; // 是否登入成功
+}
