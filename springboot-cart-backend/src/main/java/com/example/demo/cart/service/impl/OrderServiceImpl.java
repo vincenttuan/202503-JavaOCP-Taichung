9573af6c4d83a3e502ca7290fa0ed004e802e77d
FP <FP@FP999>
2025-10-14 20:46:52 +0800

    cart

--- springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/OrderServiceImpl.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/OrderServiceImpl.java
@@ -11,6 +11,7 @@ import com.example.demo.cart.exception.UserNotFoundException;
 import com.example.demo.cart.model.dto.OrderDTO;
 import com.example.demo.cart.model.dto.OrderItemDTO;
 import com.example.demo.cart.model.entity.Order;
+import com.example.demo.cart.model.entity.OrderItem;
 import com.example.demo.cart.model.entity.User;
 import com.example.demo.cart.repository.OrderRepository;
 import com.example.demo.cart.repository.ProductRepository;
@@ -43,10 +44,10 @@ public class OrderServiceImpl implements OrderService {
 	}
 
 	@Override
-	public OrderDTO saveOrder(Long userId, List<OrderItemDTO> orderItems)
+	public OrderDTO saveOrder(Long userId, List<OrderItemDTO> items)
 			throws UserNotFoundException, OrderItemEmptyException {
 		// 0. 判斷 orderItems 是否是空的 ?
-		if(orderItems == null || orderItems.isEmpty()) {
+		if(items == null || items.isEmpty()) {
 			throw new OrderItemEmptyException("無訂單項目資料");
 		}
 		
@@ -56,12 +57,21 @@ public class OrderServiceImpl implements OrderService {
 		// 2. 建立訂單 order
 		Order order = new Order();
 		
-		
 		// 3. 設定 order 與 user 的關係
+		order.setUser(user);
 		
 		// 4. 建立訂單明細 orderItems
+		List<OrderItem> orderItems = items.stream()
+				.map(item -> {
+					OrderItem orderItem = modelMapper.map(item, OrderItem.class);
+					// orderItem 與 order 關係
+					orderItem.setOrder(order);
+					return orderItem;
+				})
+				.toList();
 		
 		// 5. 設定 order 與 orderItems 的關係
+		order.setOrderItems(orderItems);
 		
 		// 6. 保存 order
 		
