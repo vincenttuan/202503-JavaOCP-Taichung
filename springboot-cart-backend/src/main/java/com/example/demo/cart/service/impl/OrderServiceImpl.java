22e457e339ae32882733cd80f2b724daa427ea92
FP <FP@FP999>
2025-10-14 19:34:10 +0800

    cart

---
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/OrderServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.demo.cart.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.cart.exception.OrderItemEmptyException;
+import com.example.demo.cart.exception.UserNotFoundException;
+import com.example.demo.cart.model.dto.OrderDTO;
+import com.example.demo.cart.model.dto.OrderItemDTO;
+import com.example.demo.cart.service.OrderService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+	@Override
+	public List<OrderDTO> findOrdersByUserId(Long userId) throws UserNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderDTO saveOrder(Long userId, List<OrderItemDTO> orderItems)
+			throws UserNotFoundException, OrderItemEmptyException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
