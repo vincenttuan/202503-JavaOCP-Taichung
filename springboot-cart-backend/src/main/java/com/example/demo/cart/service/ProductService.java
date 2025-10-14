69ddb8b4408e4077e542c5d5c23ae66ac9df9ec8
FP <FP@FP999>
2025-10-14 18:38:26 +0800

    cart

---
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.example.demo.cart.service;
+
+import java.util.List;
+
+import com.example.demo.cart.exception.AddException;
+import com.example.demo.cart.exception.ProductNotFoundException;
+import com.example.demo.cart.model.dto.ProductDTO;
+
+public interface ProductService {
+	// 取得所有商品
+	List<ProductDTO> getAllProducts();
+	
+	// 取得指定商品
+	ProductDTO getProductById(Integer id) throws ProductNotFoundException;
+	
+	// 新增商品
+	ProductDTO saveProduct(ProductDTO productDTO) throws AddException;
+	
+}
