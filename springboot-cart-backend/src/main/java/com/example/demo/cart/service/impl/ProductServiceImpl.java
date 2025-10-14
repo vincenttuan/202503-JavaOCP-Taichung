cfa41cb5cf3a86fd0d73a54f0fdfb20d48988a2b
FP <FP@FP999>
2025-10-14 18:42:12 +0800

    cart

--- springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/ProductServiceImpl.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/ProductServiceImpl.java
@@ -2,16 +2,25 @@ package com.example.demo.cart.service.impl;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.cart.exception.AddException;
 import com.example.demo.cart.exception.ProductNotFoundException;
 import com.example.demo.cart.model.dto.ProductDTO;
+import com.example.demo.cart.repository.ProductRepository;
 import com.example.demo.cart.service.ProductService;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@Override
 	public List<ProductDTO> getAllProducts() {
 		// TODO Auto-generated method stub
