7ad339a5496cd92d614f938c1abecd93fada20a2
FP <FP@FP999>
2025-10-14 18:41:24 +0800

    cart

---
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/ProductServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.cart.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.cart.exception.AddException;
+import com.example.demo.cart.exception.ProductNotFoundException;
+import com.example.demo.cart.model.dto.ProductDTO;
+import com.example.demo.cart.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public List<ProductDTO> getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductDTO getProductById(Integer id) throws ProductNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductDTO saveProduct(ProductDTO productDTO) throws AddException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
