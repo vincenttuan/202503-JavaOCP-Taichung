c7eed337b5f3da8da8d6898ee56f45beeb4a2c69
FP <FP@FP999>
2025-10-21 21:00:57 +0800

    cart

--- springboot-cart-backend/src/main/java/com/example/demo/cart/controller/FavoriteController.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/controller/FavoriteController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.SpringbootCartBackendApplication;
+import com.example.demo.cart.exception.ProductNotFoundException;
 import com.example.demo.cart.exception.UserNotFoundException;
 import com.example.demo.cart.model.dto.FavoriteProductDTO;
+import com.example.demo.cart.model.dto.FavoriteUserDTO;
 import com.example.demo.cart.model.dto.UserDTO;
 import com.example.demo.cart.repository.UserRepository;
 import com.example.demo.cart.response.ApiResponse;
@@ -21,18 +24,24 @@ import jakarta.servlet.http.HttpSession;
  * FavoriteController:
  * Request Mapping: "/favorites"
  * ---------------------------------------------------------------------------------------------------
- * GET    "", "/"               獲取用戶(已登入)關注清單 範例: "/favorites" 或 "/favorites/"
- * GET    "/product/{productId} 獲取該商品被那些用戶所關注 範例: "/favorites/product/1" 或 "/favorites/product/2"
- * POST   "/{productId}        用戶(已登入)加入所關注的商品 範例: "/favorites/1" 或 "/favorites/2"
- * DELETE "/{productId}        用戶(已登入)移除所關注的商品 範例: "/favorites/1" 或 "/favorites/2"
+ * GET    "", "/"                獲取用戶(已登入)關注清單 範例: "/favorites" 或 "/favorites/"
+ * GET    "/product/{productId}" 獲取該商品被那些用戶所關注 範例: "/favorites/product/1" 或 "/favorites/product/2"
+ * POST   "/{productId}"         用戶(已登入)加入所關注的商品 範例: "/favorites/1" 或 "/favorites/2"
+ * DELETE "/{productId}"         用戶(已登入)移除所關注的商品 範例: "/favorites/1" 或 "/favorites/2"
  * */
 @RestController
 @RequestMapping("/favorites")
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class FavoriteController {
 
+    private final UserRepository userRepository;
+
     @Autowired
 	private UserService userService;
+
+    FavoriteController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     // 獲取用戶(已登入)關注清單
 	@GetMapping(value = {"", "/"})
@@ -55,6 +64,17 @@ public class FavoriteController {
 	}
 	
 	// 獲取該商品被那些用戶所關注
+	@GetMapping("/product/{productId}")
+	public ApiResponse<List<FavoriteUserDTO>> getFavoriteUsers(@PathVariable Long productId) {
+		List<FavoriteUserDTO> favoriteUserDTOs = null;
+		try {
+			favoriteUserDTOs = userService.getFavoriteUsers(productId);
+		} catch (ProductNotFoundException e) {
+			return new ApiResponse<>(400, "查無商品", null);
+		}
+		return new ApiResponse<>(200, "查詢成功", favoriteUserDTOs);
+	}
+	
 	
 	// 用戶(已登入)加入所關注的商品
 	
