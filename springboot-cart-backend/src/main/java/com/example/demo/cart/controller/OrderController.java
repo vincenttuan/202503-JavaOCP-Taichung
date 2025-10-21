35f90872e96be07ab7e6d69793154c4feb72dcb7
FP <FP@FP999>
2025-10-21 19:43:27 +0800

    cart

--- springboot-cart-backend/src/main/java/com/example/demo/cart/controller/OrderController.java
+++ springboot-cart-backend/src/main/java/com/example/demo/cart/controller/OrderController.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.cart.exception.OrderItemEmptyException;
 import com.example.demo.cart.exception.UserNotFoundException;
 import com.example.demo.cart.model.dto.OrderDTO;
+import com.example.demo.cart.model.dto.OrderItemDTO;
 import com.example.demo.cart.model.dto.UserDTO;
 import com.example.demo.cart.response.ApiResponse;
 import com.example.demo.cart.service.OrderService;
@@ -35,7 +39,7 @@ public class OrderController {
 	public ApiResponse<List<OrderDTO>> getAllOrders(HttpSession httpSession) {
 		// 是否有登入資訊
 		if(httpSession.getAttribute("userDTO") == null) {
-			return new ApiResponse<>(400, "無登入資料", null);
+			return new ApiResponse<>(400, "無登入資料, 請先登入", null);
 		}
 		
 		UserDTO userDTO = (UserDTO)httpSession.getAttribute("userDTO");
@@ -46,6 +50,27 @@ public class OrderController {
 			return new ApiResponse<>(400, "查無使用者", null);
 		}
 		return new ApiResponse<>(200, "查詢成功", orderDTOs);
+	}
+	
+	@PostMapping("/checkout")
+	public ApiResponse<OrderDTO> checkout(@RequestBody List<OrderItemDTO> orderItems, HttpSession httpSession) {
+		// 是否有登入資訊
+		if(httpSession.getAttribute("userDTO") == null) {
+			return new ApiResponse<>(400, "無登入資料, 請先登入", null);
+		}
+		
+		UserDTO userDTO = (UserDTO)httpSession.getAttribute("userDTO");
+		OrderDTO orderDTO = null;
+		try {
+			orderDTO = orderService.saveOrder(userDTO.getId(), orderItems);
+		} catch (UserNotFoundException e1) {
+			return new ApiResponse<>(400, e1.getMessage(), null);
+		} catch (OrderItemEmptyException e2) {
+			return new ApiResponse<>(400, e2.getMessage(), null);
+		}
+		
+		new ApiResponse<>(200, "結帳成功", orderDTO);
+		
 	}
 	
 }
