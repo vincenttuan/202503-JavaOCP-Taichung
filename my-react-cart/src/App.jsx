5e5d0497ae2eb1d9f9f78f574b6a857e822adeba
FP <FP@FP999>
2025-10-23 19:50:16 +0800

    cart

--- my-react-cart/src/App.jsx
+++ my-react-cart/src/App.jsx
@@ -3,13 +3,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Footer from "./components/Footer"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      Main Page
-    </>
+    <Router>
+      {/* 導航列-位於最上方 */}
+      <Navbar />
+
+      {/* 主要內容區-位於中間部分 */}
+      <div className='content'>
+        <Routes>
+          {/* 首頁路由 */}
+          <Route path="/" element={<Home />} />
+
+          {/* 商品路由 */}
+
+          {/* 購物車路由 */}
+
+          {/* 登入路由 */}
+
+        </Routes>
+      </div>
+
+      {/* 頁尾-位於最下方 */}
+      <Footer />
+
+    </Router>
   )
 }
 
